15ac744a63265f31ce5dee660886e008db07664c
Ben Greear <greearb@candelatech.com>
2014-03-05 15:16:17 -0800

    wpa_supplicant: Support minimum interval between scan attempts.
    When running lots of vifs, they can all end up wanting to
    scan right after the other.  This leaves too little time for
    useful work to happen.  Allow users to configure a minimum
    timer interval (in seconds) between scans.  Default is zero,
    which is current behaviour.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -4113,6 +4113,7 @@ struct wpa_config * wpa_config_alloc_empty(const char *ctrl_interface,
 	config->key_mgmt_offload = DEFAULT_KEY_MGMT_OFFLOAD;
 	config->cert_in_cb = DEFAULT_CERT_IN_CB;
 	config->wpa_rsc_relaxation = DEFAULT_WPA_RSC_RELAXATION;
+	config->min_scan_gap = DEFAULT_MIN_SCAN_GAP;
 
 #ifdef CONFIG_MBO
 	config->mbo_cell_capa = DEFAULT_MBO_CELL_CAPA;
@@ -4740,6 +4741,7 @@ static const struct global_parse_data global_fields[] = {
 	{ INT(dot11RSNAConfigPMKLifetime), 0 },
 	{ INT(dot11RSNAConfigPMKReauthThreshold), 0 },
 	{ INT(dot11RSNAConfigSATimeout), 0 },
+	{ INT(min_scan_gap), 0 },
 #ifndef CONFIG_NO_CONFIG_WRITE
 	{ INT(update_config), 0 },
 #endif /* CONFIG_NO_CONFIG_WRITE */
