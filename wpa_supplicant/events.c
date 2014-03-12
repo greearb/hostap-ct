1f60faebbe1ada969a0113ce244f4b65ca703a39
Ben Greear <greearb@candelatech.com>
2014-03-12 13:40:44 -0700

    supplicant: Assume external scans complete within 30 seconds.
    When testing relatively buggy ath10k driver and firmware,
    I saw cases where supplicant thought external scans were
    active when in fact none were.  This effectively hangs supplicant
    because it will never again scan to start the association logic.
    
    This patch times out external scans after 30 seconds and continues
    on as if the external scan had completed.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -4662,6 +4662,7 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 		} else {
 			wpa_dbg(wpa_s, MSG_DEBUG, "External program started a scan");
 			wpa_s->radio->external_scan_running = 1;
+			os_get_reltime(&wpa_s->external_scan_start_time);
 			wpa_msg_ctrl(wpa_s, MSG_INFO, WPA_EVENT_SCAN_STARTED);
 		}
 		break;
