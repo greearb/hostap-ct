b640a5759ae46b5688128aee76d784f85f1a3425
Ben Greear <greearb@candelatech.com>
2015-03-26 17:13:20 -0400

    scan: Properly cancel scans.
    You need to remove work items as well as cancel events.
    Also, reorder some freq selection logic.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/scan.c
+++ wpa_supplicant/scan.c
@@ -192,6 +192,8 @@ static void wpas_trigger_scan_cb(struct wpa_radio_work *work, int deinit)
 		}
 		wpa_supplicant_notify_scanning(wpa_s, 0);
 		wpas_notify_scan_done(wpa_s, 0);
+                wpa_dbg(wpa_s, MSG_DEBUG, "trigger-scan-cb, deinit, work: %p",
+                        wpa_s->scan_work);
 		wpa_s->scan_work = NULL;
 		return;
 	}
@@ -271,6 +273,8 @@ static void wpas_trigger_scan_cb(struct wpa_radio_work *work, int deinit)
 	wpa_s->own_scan_requested = 1;
 	wpa_s->clear_driver_scan_cache = 0;
 	wpa_s->scan_work = work;
+        wpa_dbg(wpa_s, MSG_DEBUG, "trigger-scan-cb, assign work: %p",
+                wpa_s->scan_work);
 }
 
 
@@ -290,6 +294,8 @@ int wpa_supplicant_trigger_scan(struct wpa_supplicant *wpa_s,
 		return -1;
 	}
 
+        wpa_dbg(wpa_s, MSG_DEBUG, "trigger-scan, freqs: %p",
+                params->freqs);
 	ctx = wpa_scan_clone_params(params);
 	if (!ctx ||
 	    radio_add_work(wpa_s, 0, "scan", 0, wpas_trigger_scan_cb, ctx) < 0)
@@ -302,6 +308,16 @@ int wpa_supplicant_trigger_scan(struct wpa_supplicant *wpa_s,
 	return 0;
 }
 
+void wpa_remove_scan_work(struct wpa_supplicant *wpa_s, const char* dbg, int own_request)
+{
+	if (own_request && wpa_s->scan_work) {
+		struct wpa_radio_work *work = wpa_s->scan_work;
+                wpa_dbg(wpa_s, MSG_DEBUG, "Remove scan work, work: %p dbg: %s",
+                        work, dbg);
+		wpa_s->scan_work = NULL;
+		radio_work_done(work);
+	}
+}
 
 static void
 wpa_supplicant_delayed_sched_scan_timeout(void *eloop_ctx, void *timeout_ctx)
@@ -1224,19 +1240,6 @@ ssid_list_set:
 	} else
 		os_free(wpa_s->next_scan_freqs);
 	wpa_s->next_scan_freqs = NULL;
-	wpa_setband_scan_freqs(wpa_s, &params);
-
-	/* See if user specified frequencies. If so, scan only those. */
-	if (wpa_s->last_scan_req == INITIAL_SCAN_REQ &&
-	    wpa_s->conf->initial_freq_list && !params.freqs) {
-		wpa_dbg(wpa_s, MSG_DEBUG,
-			"Optimize scan based on conf->initial_freq_list");
-		int_array_concat(&params.freqs, wpa_s->conf->initial_freq_list);
-	} else if (wpa_s->conf->freq_list && !params.freqs) {
-		wpa_dbg(wpa_s, MSG_DEBUG,
-			"Optimize scan based on conf->freq_list");
-		int_array_concat(&params.freqs, wpa_s->conf->freq_list);
-	}
 
 	/* Use current associated channel? */
 	if (wpa_s->conf->scan_cur_freq && !params.freqs) {
@@ -1260,6 +1263,20 @@ ssid_list_set:
 	if (wpa_s->enable_oce & OCE_STA)
 		params.oce_scan = 1;
 #endif /* CONFIG_MBO */
+
+	wpa_setband_scan_freqs(wpa_s, &params);
+
+	/* See if user specified frequencies. If so, scan only those. */
+	if (wpa_s->last_scan_req == INITIAL_SCAN_REQ &&
+	    wpa_s->conf->initial_freq_list && !params.freqs) {
+		wpa_dbg(wpa_s, MSG_DEBUG,
+			"Optimize scan based on conf->initial_freq_list");
+		int_array_concat(&params.freqs, wpa_s->conf->initial_freq_list);
+	} else if (wpa_s->conf->freq_list && !params.freqs) {
+		wpa_dbg(wpa_s, MSG_DEBUG,
+			"Optimize scan based on conf->freq_list");
+		int_array_concat(&params.freqs, wpa_s->conf->freq_list);
+	}
 
 	params.filter_ssids = wpa_supplicant_build_filter_ssids(
 		wpa_s->conf, &params.num_filter_ssids);
@@ -1803,6 +1820,9 @@ void wpa_supplicant_cancel_scan(struct wpa_supplicant *wpa_s)
 {
 	wpa_dbg(wpa_s, MSG_DEBUG, "Cancelling scan request");
 	eloop_cancel_timeout(wpa_supplicant_scan, wpa_s, NULL);
+
+        radio_remove_works(wpa_s, "scan", 0);
+	wpa_remove_scan_work(wpa_s, "cancel-scan", 1);
 }
 
 
@@ -2768,11 +2788,8 @@ void scan_only_handler(struct wpa_supplicant *wpa_s,
 	}
 	wpas_notify_scan_results(wpa_s);
 	wpas_notify_scan_done(wpa_s, 1);
-	if (wpa_s->scan_work) {
-		struct wpa_radio_work *work = wpa_s->scan_work;
-		wpa_s->scan_work = NULL;
-		radio_work_done(work);
-	}
+
+	wpa_remove_scan_work(wpa_s, "scan-only-handler", 1);
 
 	if (wpa_s->wpa_state == WPA_SCANNING)
 		wpa_supplicant_set_state(wpa_s, wpa_s->scan_prev_wpa_state);
