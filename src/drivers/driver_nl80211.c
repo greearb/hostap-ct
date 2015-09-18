be087f5e0de4c5fc3d45e152c501265294f759fa
Ben Greear <greearb@candelatech.com>
2015-09-18 17:24:35 -0400

    nl80211: Fix typo in logging messages.
    Fix typo.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -6609,7 +6609,7 @@ static int wpa_driver_nl80211_associate(
 		nl80211_dump_scan(drv);
 	} else {
 		wpa_printf(MSG_DEBUG,
-			   "nl80211: Association request send successfully");
+			   "nl80211: Association request sent successfully");
 	}
 
 fail:
