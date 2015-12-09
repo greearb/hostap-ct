f00d57d05e7d1f7644ee6357f00e995c3127d37b
Ben Greear <greearb@candelatech.com>
2015-12-09 12:20:33 -0800

    hostapd: Take configured antenna set into consideration
    If user has configured TX antennas to be less than what hardware
    advertises, the MCS reported by hardware will be too large. So, remove
    MCS sets accordingly.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- src/ap/ieee802_11_vht.c
+++ src/ap/ieee802_11_vht.c
@@ -44,6 +44,7 @@ u8 * hostapd_eid_vht_capabilities(struct hostapd_data *hapd, u8 *eid, u32 nsts)
 	struct ieee80211_vht_capabilities *cap;
 	struct hostapd_hw_modes *mode = hapd->iface->current_mode;
 	u8 *pos = eid;
+	int i;
 
 	if (!mode || is_6ghz_op_class(hapd->iconf->op_class))
 		return eid;
@@ -72,6 +73,26 @@ u8 * hostapd_eid_vht_capabilities(struct hostapd_data *hapd, u8 *eid, u32 nsts)
 
 	/* Supported MCS set comes from hw */
 	os_memcpy(&cap->vht_supported_mcs_set, mode->vht_mcs_set, 8);
+
+	/*
+	 * Disable all MCS rates that are out of the configured nss (antenna)
+	 * range.
+	 */
+	if (mode->conf_tx_ant) {
+		for (i = 7; i > 0; i--) {
+			if (mode->conf_tx_ant & BIT(i - 1))
+				break;
+			cap->vht_supported_mcs_set.tx_map |= (0x3 << i);
+		}
+	}
+
+	if (mode->conf_rx_ant) {
+		for (i = 7; i > 0; i--) {
+			if (mode->conf_rx_ant & BIT(i - 1))
+				break;
+			cap->vht_supported_mcs_set.rx_map |= (0x3 << i);
+		}
+	}
 
 	pos += sizeof(*cap);
 
