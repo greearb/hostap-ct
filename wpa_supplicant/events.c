c721065830f31f177eca8b6559b966dfcf5febf0
Ben Greear <greearb@candelatech.com>
2016-03-22 19:10:54 -0400

    supplicant: Allow disabling reassoc based on scan results.
    This gives configurable control over whether to consider
    roaming based on scan results.  I find this useful when
    doing explicit roaming tests, where I do not want scan
    requests to cause the roam automatically.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -2171,6 +2171,12 @@ int wpa_supplicant_need_to_roam_within_ess(struct wpa_supplicant *wpa_s,
 	size_t sel_ie_len = selected->ie_len ? selected->ie_len :
 		selected->beacon_ie_len;
 
+	if (wpa_s->conf->disable_ess_roaming) {
+		wpa_dbg(wpa_s, MSG_DEBUG,
+			"NOT considering within-ESS reassociation, disable_ess_roaming is true");
+		return 0;
+	}
+
 	wpa_dbg(wpa_s, MSG_DEBUG, "Considering within-ESS reassociation");
 	wpa_dbg(wpa_s, MSG_DEBUG, "Current BSS: " MACSTR
 		" freq=%d level=%d snr=%d est_throughput=%u",
