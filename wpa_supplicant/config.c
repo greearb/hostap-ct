a7e50df0bd5767edcc4d4463537ca81541e886ab
Ben Greear <greearb@candelatech.com>
2016-03-22 19:10:54 -0400

    supplicant: Allow disabling reassoc based on scan results.
    This gives configurable control over whether to consider
    roaming based on scan results.  I find this useful when
    doing explicit roaming tests, where I do not want scan
    requests to cause the roam automatically.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -4468,6 +4468,9 @@ struct wpa_config * wpa_config_alloc_empty(const char *ctrl_interface,
 		config->driver_param = os_strdup(driver_param);
 	config->gas_rand_addr_lifetime = DEFAULT_RAND_ADDR_LIFETIME;
 
+#ifndef CONFIG_NO_ROAMING
+	config->disable_ess_roaming = DEFAULT_DISABLE_ESS_ROAMING;
+#endif
 	config->concurrent_assoc_ok = DEFAULT_CONCURRENT_ASSOC_OK;
 	config->accept_external_scan_results = DEFAULT_ACCEPT_EXTERNAL_SCAN_RESULTS;
 
@@ -5273,6 +5276,9 @@ static const struct global_parse_data global_fields[] = {
 	{ INT(tdls_external_control), 0},
 	{ STR(osu_dir), 0 },
 	{ STR(wowlan_triggers), CFG_CHANGED_WOWLAN_TRIGGERS },
+#ifndef CONFIG_NO_ROAMING
+	{ INT(disable_ess_roaming), 0 },
+#endif
 	{ INT(concurrent_assoc_ok), 0 },
 	{ INT(accept_external_scan_results), 0 },
 	{ INT(p2p_search_delay), 0},
