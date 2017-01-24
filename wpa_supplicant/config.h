add347a7c7bf0e8681266cb51be792e7f8aa5180
Ben Greear <greearb@candelatech.com>
2017-01-24 15:20:30 -0500

    supplicant: Allow configuring percentage for ignore_auth_resp
    This lets one configure a station to drop a percentage
    (including 0 and 100%) of the RX management frames to
    aid testing applications.
    
    The value is moved to the config section so that it can be
    defined in a supplicant config file.  There is a functional
    change in that before you could reset a station through
    wpa_cli and it would clear the ignore_auth_resp value,
    and now you would have to explicitly clear it since it is
    a configuration.
    
    To enable the feature, you must compile with CONFIG_TESTING_OPTIONS
    enabled.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/config.h
+++ wpa_supplicant/config.h
@@ -52,6 +52,7 @@
 #define DEFAULT_OCE_SUPPORT OCE_STA
 #define DEFAULT_EXTENDED_KEY_ID 0
 #define DEFAULT_SCAN_RES_VALID_FOR_CONNECT 5
+#define DEFAULT_IGNORE_AUTH_RESP 0
 
 #include "config_ssid.h"
 #include "wps/wps.h"
@@ -1023,6 +1024,13 @@ struct wpa_config {
 	 * scan results.
 	 */
 	int accept_external_scan_results;
+
+#if CONFIG_TESTING_OPTIONS
+	/* Allow users to configure supplicant to drop a percentage of management frames.
+	 * 0 == never, 65535 == always
+	 */
+	unsigned short ignore_auth_resp;
+#endif
 
 	/**
 	 * disassoc_low_ack - Disassociate stations with massive packet loss
--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -746,7 +746,7 @@ static int wpa_supplicant_ctrl_iface_set(struct wpa_supplicant *wpa_s,
 	} else if (os_strcasecmp(cmd, "p2p_go_csa_on_inv") == 0) {
 		wpa_s->p2p_go_csa_on_inv = !!atoi(value);
 	} else if (os_strcasecmp(cmd, "ignore_auth_resp") == 0) {
-		wpa_s->ignore_auth_resp = !!atoi(value);
+		wpa_s->conf->ignore_auth_resp = atoi(value);
 	} else if (os_strcasecmp(cmd, "ignore_assoc_disallow") == 0) {
 		wpa_s->ignore_assoc_disallow = !!atoi(value);
 		wpa_drv_ignore_assoc_disallow(wpa_s,
@@ -8613,7 +8613,6 @@ static void wpa_supplicant_ctrl_iface_flush(struct wpa_supplicant *wpa_s)
 	wpa_s->extra_roc_dur = 0;
 	wpa_s->test_failure = WPAS_TEST_FAILURE_NONE;
 	wpa_s->p2p_go_csa_on_inv = 0;
-	wpa_s->ignore_auth_resp = 0;
 	wpa_s->ignore_assoc_disallow = 0;
 	wpa_s->disable_sa_query = 0;
 	wpa_s->testing_resend_assoc = 0;
