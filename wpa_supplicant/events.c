22712c8feb0c7a9e4508df3001c0901cb3b48423
Ben Greear <greearb@candelatech.com>
2017-05-30 14:57:45 -0700

    supplicant:  Fix and document concurrent_assoc_ok
    It had been broken on purpose back in 2016 it seems,
    but since ath10k/ath9k and do multiple assoc concurrently,
    then allow this to work if so configured.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -1898,7 +1898,11 @@ static int _wpa_supplicant_event_scan_results(struct wpa_supplicant *wpa_s,
 		scan_res_handler = wpa_s->scan_res_handler;
 		wpa_s->scan_res_handler = NULL;
 		scan_res_handler(wpa_s, scan_res);
-		ret = 1;
+
+		if (wpa_s->conf->concurrent_assoc_ok)
+			ret = 0;
+		else
+			ret = 1;
 		goto scan_work_done;
 	}
 
@@ -2054,6 +2058,10 @@ static int wpas_select_network_from_last_scan(struct wpa_supplicant *wpa_s,
 		}
 		if (new_scan)
 			wpa_supplicant_rsn_preauth_scan_results(wpa_s);
+
+		if (wpa_s->conf->concurrent_assoc_ok)
+			return 0;
+
 		/*
 		 * Do not allow other virtual radios to trigger operations based
 		 * on these scan results since we do not want them to start
@@ -2118,6 +2126,8 @@ static int wpas_select_network_from_last_scan(struct wpa_supplicant *wpa_s,
 				wpa_s->network_select = 1;
 				wpa_s->auto_network_select = 1;
 				interworking_start_fetch_anqp(wpa_s);
+				if (wpa_s->conf->concurrent_assoc_ok)
+					return 0;
 				return 1;
 			}
 #endif /* CONFIG_INTERWORKING */
@@ -2192,7 +2202,7 @@ static int wpa_supplicant_event_scan_results(struct wpa_supplicant *wpa_s,
 			 radio_list) {
 		if (ifs != wpa_s) {
 			wpa_dbg(wpa_s, MSG_DEBUG, "Updating scan results for "
-				"sibling: %s", ifs->ifname);
+				"sibling: %s, res: %d", ifs->ifname, res);
 			res = _wpa_supplicant_event_scan_results(ifs, data, 0,
 								 res > 0);
 			if (res < 0)
