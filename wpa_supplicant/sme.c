a7b861f1f22b2bc9c4117061babdc86579473df0
Ben Greear <greearb@candelatech.com>
2018-08-24 11:53:34 -0700

    supplicant: Support disabling the supported-oper-class IE.
    This provides a work-around for cases where the AP (most likely)
    has bugs when this element exists and/or has 5Ghz elements when
    the AP is evidently unexpecting that.
    
    Test case that required this fix is a DPC3941 AP, configured with
    2.4G radio to have WPA2 PSK.  Client station is ath10k 9880.
    The symptom is that the client radio does not ack the 1/4 eapol.
    I do not know exactly why, but maybe it is corrupted in some suble
    way that I did not notice.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/sme.c
+++ wpa_supplicant/sme.c
@@ -881,10 +881,12 @@ static void sme_send_authentication(struct wpa_supplicant *wpa_s,
 	sme_auth_handle_rrm(wpa_s, bss);
 
 #ifndef CONFIG_NO_RRM
-	wpa_s->sme.assoc_req_ie_len += wpas_supp_op_class_ie(
-		wpa_s, ssid, bss,
-		wpa_s->sme.assoc_req_ie + wpa_s->sme.assoc_req_ie_len,
-		sizeof(wpa_s->sme.assoc_req_ie) - wpa_s->sme.assoc_req_ie_len);
+	if (!wpa_s->conf->no_oper_classes_ie) {
+		wpa_s->sme.assoc_req_ie_len += wpas_supp_op_class_ie(
+			wpa_s, ssid, bss,
+			wpa_s->sme.assoc_req_ie + wpa_s->sme.assoc_req_ie_len,
+			sizeof(wpa_s->sme.assoc_req_ie) - wpa_s->sme.assoc_req_ie_len);
+	}
 #endif /* CONFIG_NO_RRM */
 
 	if (params.p2p)
