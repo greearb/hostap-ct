4ea3ef834b811b179a6b8bf5580296e344e2ad52
Ben Greear <greearb@candelatech.com>
2018-11-30 16:41:54 -0800

    supplicant: Prepare for EAPOL corruptions.
    This is used for testing purposes.

--- src/eap_peer/eap_i.h
+++ src/eap_peer/eap_i.h
@@ -385,6 +385,10 @@ struct eap_sm {
 	unsigned int use_machine_cred:1;
 
 	struct dl_list erp_keys; /* struct eap_erp_key */
+
+#ifdef CONFIG_TESTING_OPTIONS
+	u16 corrupt_eapol_id_resp;
+#endif
 };
 
 const u8 * eap_get_config_identity(struct eap_sm *sm, size_t *len);
