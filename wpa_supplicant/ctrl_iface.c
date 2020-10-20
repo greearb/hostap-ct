b364df16bbcf81485e5e2006f14abd9f318738fd
Ben Greear <greearb@candelatech.com>
2020-10-20 11:44:47 -0700

    /home/greearb/tmp/hap-ath11k/b00-033-wpa_supplicant-don-t-initate-TDLS-connection-if-stat.patch
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -1063,6 +1063,12 @@ static int wpa_supplicant_ctrl_iface_tdls_discover(
 	u8 peer[ETH_ALEN];
 	int ret;
 
+	if (wpa_s->wpa_state != WPA_COMPLETED || wpa_s->current_ssid == NULL) {
+		wpa_printf(MSG_DEBUG, "CTRL_IFACE TDLS_SETUP : ignoring request "
+			   "not associated!");
+		return -ENOTCONN;
+	}
+
 	if (hwaddr_aton(addr, peer)) {
 		wpa_dbg(wpa_s, MSG_DEBUG, "CTRL_IFACE TDLS_DISCOVER: invalid "
 			   "address '%s'", addr);
@@ -1086,6 +1092,12 @@ static int wpa_supplicant_ctrl_iface_tdls_setup(
 {
 	u8 peer[ETH_ALEN];
 	int ret;
+
+	if (wpa_s->wpa_state != WPA_COMPLETED || wpa_s->current_ssid == NULL) {
+		wpa_printf(MSG_DEBUG, "CTRL_IFACE TDLS_SETUP : ignoring request "
+			   "not associated!");
+		return -ENOTCONN;
+	}
 
 	if (hwaddr_aton(addr, peer)) {
 		wpa_dbg(wpa_s, MSG_DEBUG, "CTRL_IFACE TDLS_SETUP: invalid "
