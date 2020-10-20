adee00c6ad1fe320bdf8fc74ac1f508438793e29
Ben Greear <greearb@candelatech.com>
2020-10-20 11:44:03 -0700

    "/home/greearb/tmp/hap-ath11k/b00-003-hostapd-Add-ieee80211ad-support-in-ctrl-iface-comman.patch"
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- src/ap/ctrl_iface_ap.c
+++ src/ap/ctrl_iface_ap.c
@@ -737,6 +737,7 @@ int hostapd_ctrl_iface_status(struct hostapd_data *hapd, char *buf,
 			  "secondary_channel=%d\n"
 			  "ieee80211n=%d\n"
 			  "ieee80211ac=%d\n"
+			  "ieee80211ad=%d\n"
 			  "ieee80211ax=%d\n"
 			  "beacon_int=%u\n"
 			  "dtim_period=%d\n",
@@ -748,6 +749,7 @@ int hostapd_ctrl_iface_status(struct hostapd_data *hapd, char *buf,
 			  iface->conf->ieee80211n && !hapd->conf->disable_11n,
 			  iface->conf->ieee80211ac &&
 			  !hapd->conf->disable_11ac,
+			  iface->conf->hw_mode == HOSTAPD_MODE_IEEE80211AD,
 			  iface->conf->ieee80211ax &&
 			  !hapd->conf->disable_11ax,
 			  iface->conf->beacon_int,
