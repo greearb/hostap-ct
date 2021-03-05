2ea063168d81d0993d0f029209465fecfcac826a
Ben Greear <greearb@candelatech.com>
2021-03-04 16:32:05 -0800

    supplicant: Enable disabling twt
    Used for test purposes primarily, can disable TWT in stations, even if their
    radio supports it.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/config_file.c
+++ wpa_supplicant/config_file.c
@@ -1015,6 +1015,7 @@ static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid,
 #endif /* CONFIG_VHT_OVERRIDES */
 #ifdef CONFIG_HE_OVERRIDES
 	INT(disable_he);
+	INT(disable_twt);
 #endif /* CONFIG_HE_OVERRIDES */
 	INT(disable_eht);
 	INT(disable_uhr);
