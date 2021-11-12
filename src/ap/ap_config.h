5b0110041f90b13cbeb0574363e487b98994d605
shivam <shivam.thakur@candelatech.com>
2021-11-12 15:24:07 +0530

    hostap : EAPOL 1/4 and 3/4 Packet corruption for Testing Purpose MSG 1/4 and 3/4 during 4-WAY HS can be corrupted with the given probability

--- src/ap/ap_config.h
+++ src/ap/ap_config.h
@@ -1141,6 +1141,9 @@ struct hostapd_config {
 	double ignore_assoc_probability;
 	double ignore_reassoc_probability;
 	double corrupt_gtk_rekey_mic_probability;
+	/* Corrupt Packets */
+	double corrupt_eapol_1_of_4;
+	double corrupt_eapol_3_of_4;
 	int ecsa_ie_only;
 	bool delay_eapol_tx;
 #endif /* CONFIG_TESTING_OPTIONS */
