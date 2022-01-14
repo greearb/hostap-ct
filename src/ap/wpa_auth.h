6d4dc25dc7022de8b660a3c90190d54b5d69a83c
shivam <shivam.thakur@candelatech.com>
2022-01-14 21:41:02 +0530

    Hostap: Added EAPOL 1/4 and 3/4 Packet Drop
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

--- src/ap/wpa_auth.h
+++ src/ap/wpa_auth.h
@@ -230,6 +230,8 @@ struct wpa_auth_config {
 	double corrupt_gtk_rekey_mic_probability;
 	double corrupt_eapol_1_of_4;
 	double corrupt_eapol_3_of_4;
+	double drop_eapol_1_of_4;
+	double drop_eapol_3_of_4;
 	u8 own_ie_override[MAX_OWN_IE_OVERRIDE];
 	size_t own_ie_override_len;
 	bool rsne_override_set;
