d53238753b94f51cbb56470535b2d5f43bb2cd09
Ben Greear <greearb@candelatech.com>
2022-06-16 14:16:31 -0700

    Allow compiling on F21 and earlier.
    Add define that can be passed in CFLAGS to allow compiling out things
    not supported on Fedora21 and earlier openssl.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- src/crypto/tls_openssl.c
+++ src/crypto/tls_openssl.c
@@ -5690,6 +5690,7 @@ int tls_connection_set_params(void *tls_ctx, struct tls_connection *conn,
 	if (!params->openssl_ecdh_curves) {
 #ifndef OPENSSL_IS_BORINGSSL
 #ifndef OPENSSL_NO_EC
+#ifndef HAP_OPENSSL_NO_EC
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
 		if (SSL_set_ecdh_auto(conn->ssl, 1) != 1) {
 			wpa_printf(MSG_INFO,
@@ -5697,6 +5698,7 @@ int tls_connection_set_params(void *tls_ctx, struct tls_connection *conn,
 			return -1;
 		}
 #endif /* < 1.1.0 */
+#endif /* HAP_OPENSSL_NO_EC */
 #endif /* OPENSSL_NO_EC */
 #endif /* OPENSSL_IS_BORINGSSL */
 	} else if (params->openssl_ecdh_curves[0]) {
@@ -5705,7 +5707,7 @@ int tls_connection_set_params(void *tls_ctx, struct tls_connection *conn,
 			"OpenSSL: ECDH configuration not supported");
 		return -1;
 #else /* !OPENSSL_IS_BORINGSSL */
-#ifndef OPENSSL_NO_EC
+#if ! (defined OPENSSL_NO_EC || defined HAP_OPENSSL_NO_EC)
 		if (SSL_set1_curves_list(conn->ssl,
 					 params->openssl_ecdh_curves) != 1) {
 			wpa_printf(MSG_INFO,
@@ -5920,7 +5922,7 @@ int tls_global_set_params(void *tls_ctx,
 
 	if (!params->openssl_ecdh_curves) {
 #ifndef OPENSSL_IS_BORINGSSL
-#ifndef OPENSSL_NO_EC
+#if ! (defined OPENSSL_NO_EC || defined HAP_OPENSSL_NO_EC)
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
 		if (SSL_CTX_set_ecdh_auto(ssl_ctx, 1) != 1) {
 			wpa_printf(MSG_INFO,
@@ -5936,7 +5938,7 @@ int tls_global_set_params(void *tls_ctx,
 			"OpenSSL: ECDH configuration not supported");
 		return -1;
 #else /* !OPENSSL_IS_BORINGSSL */
-#ifndef OPENSSL_NO_EC
+#if ! (defined OPENSSL_NO_EC || defined HAP_OPENSSL_NO_EC)
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
 		SSL_CTX_set_ecdh_auto(ssl_ctx, 1);
 #endif
