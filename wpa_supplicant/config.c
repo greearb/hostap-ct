0d05748e79f995e45597471a95ed38163a74564e
Ben Greear <greearb@candelatech.com>
2022-08-01 10:50:32 -0700

    Allows the supplicant to select and use FT over-the-DS automatically for SME base drivers, if specified in the network block config. If the option is not specified, FT over-the-air is used.
    From: https://www.spinics.net/lists/hostap/msg05809.html
    
    Signed-off-by: Jasraj Randhawa <jasraj@xxxxxxxxxxxxxxxxx>
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -2756,6 +2756,9 @@ static const struct parse_data ssid_fields[] = {
 	{ INT_RANGE(owe_only, 0, 1) },
 	{ INT_RANGE(owe_ptk_workaround, 0, 1) },
 	{ INT_RANGE(multi_ap_backhaul_sta, 0, 1) },
+#ifdef CONFIG_IEEE80211R
+	{ INT_RANGE(ft_over_ds, 0, 1) },
+#endif /* CONFIG_IEEE80211R */
 	{ INT_RANGE(ft_eap_pmksa_caching, 0, 1) },
 	{ INT_RANGE(multi_ap_profile, MULTI_AP_PROFILE_1,
 		    MULTI_AP_PROFILE_MAX) },
