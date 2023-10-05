a978aa5ecafaa8e5546f6553068abaec771ffe2b
Ben Greear <greearb@candelatech.com>
2023-10-05 16:34:48 -0700

    supplicant:  allow disabling MLO.
    MLO should be disabled in case EHT is disabled, and also add new
    configurable to disable MLO regardless of EHT settings.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/wpa_cli.c
+++ wpa_supplicant/wpa_cli.c
@@ -1508,6 +1508,7 @@ static const char *network_fields[] = {
 	"disable_ofdma",
 #endif /* CONFIG_HE_OVERRIDES */
 	"disable_eht",
+	"disable_mlo",
 	"ap_max_inactivity", "dtim_period", "beacon_int",
 #ifdef CONFIG_MACSEC
 	"macsec_policy",
