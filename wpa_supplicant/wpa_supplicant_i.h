5365d0f31a9340c904a93880ca6d2ae192b0ce98
Ben Greear <greearb@candelatech.com>
2023-10-05 10:24:55 -0700

    supplicant:  Scan without coloc-6ghz flag every 5th time.
    There may be some cases where coloc-6ghz is causing issues,
    perhaps related to scan-on-channel logic with multiple stations
    on a radio.
    
    Attempt work-around by disabling the coloc-6ghz flag every 5th
    scan when STA is disconnected.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- wpa_supplicant/wpa_supplicant_i.h
+++ wpa_supplicant/wpa_supplicant_i.h
@@ -1252,6 +1252,7 @@ struct wpa_supplicant {
 	unsigned int num_prov_found;
 #endif /* CONFIG_INTERWORKING */
 	unsigned int drv_capa_known;
+	unsigned int scan_attempts;
 
 	struct {
 		struct hostapd_hw_modes *modes;
