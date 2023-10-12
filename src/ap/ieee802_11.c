55ea6a5ee0ee64543e058df9ac3eee0715049f6b
mtk25255 <rohit.kamat@mediatek.com>
2023-10-12 14:29:23 +0800

    mtk: hostapd: Fix RSNXE Interop issue with STA

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -5635,6 +5635,7 @@ static void handle_assoc(struct hostapd_data *hapd,
 	int omit_rsnxe = 0;
 	bool set_beacon = false;
 	bool mld_addrs_not_translated = false;
+	bool sae_pk = false;
 
 	if (len < IEEE80211_HDRLEN + (reassoc ? sizeof(mgmt->u.reassoc_req) :
 				      sizeof(mgmt->u.assoc_req))) {
@@ -5874,12 +5875,27 @@ static void handle_assoc(struct hostapd_data *hapd,
 	resp = check_assoc_ies(hapd, sta, pos, left, reassoc);
 	if (resp != WLAN_STATUS_SUCCESS)
 		goto fail;
+
 #ifdef CONFIG_IEEE80211R_AP
 	if (reassoc && sta->auth_alg == WLAN_AUTH_FT)
 		omit_rsnxe = !get_ie(pos, left, WLAN_EID_RSNX);
 #endif /* CONFIG_IEEE80211R_AP */
 	if (hapd->conf->rsn_override_omit_rsnxe)
 		omit_rsnxe = 1;
+
+	/* TODO: check if this section is needed */
+	omit_rsnxe = !get_ie(pos, left, WLAN_EID_RSNX);
+#ifdef CONFIG_SAE_PK
+	sae_pk = hostapd_sae_pk_in_use(hapd->conf);
+#endif /* CONFIG_SAE_PK */
+	if (omit_rsnxe) {
+		if (!reassoc && wpa_key_mgmt_sae(hapd->conf->wpa_key_mgmt) &&
+				(hapd->conf->sae_pwe == SAE_PWE_HASH_TO_ELEMENT ||
+				 hapd->conf->sae_pwe == SAE_PWE_BOTH || sae_pk ||
+				 wpa_key_mgmt_sae_ext_key(hapd->conf->wpa_key_mgmt))) {
+			omit_rsnxe = 0;
+		}
+	}
 
 	if (hostapd_get_aid(hapd, sta) < 0) {
 		hostapd_logger(hapd, mgmt->sa, HOSTAPD_MODULE_IEEE80211,
