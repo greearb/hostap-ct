b2a343f4333a796b587c43cb584770478a428cb5
Shayne Chen <shayne.chen@mediatek.com>
2023-12-11 17:02:05 +0800

    mtk: hostapd: extend ap_get_sta() to find the correct sta
    There're still some mld address tranlation issues that need to be dealt
    with on driver side (e.g. RX eapol frames). So add the code that find
    station also with link address and across hapds at the moment.
    
    Signed-off-by: Shayne Chen <shayne.chen@mediatek.com>

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -3591,6 +3591,7 @@ static void handle_auth(struct hostapd_data *hapd,
 				  mgmt->sa, ETH_ALEN);
 			os_memcpy(sta->mld_info.links[link_id].local_addr,
 				  hapd->own_addr, ETH_ALEN);
+			os_memcpy(sta->setup_link_addr, mgmt->sa, ETH_ALEN);
 		}
 
 		if ((!(sta->flags & WLAN_STA_MFP) ||
