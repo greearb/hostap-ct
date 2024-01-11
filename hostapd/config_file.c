0fdb5c6ba15a37926301de1fb7f09079bcafefad
Ben Greear <greearb@candelatech.com>
2024-01-11 12:25:36 -0800

    hostapd:  Improve bss color related logging.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- hostapd/config_file.c
+++ hostapd/config_file.c
@@ -3620,6 +3620,8 @@ static int hostapd_config_fill(struct hostapd_config *conf,
 	} else if (os_strcmp(buf, "he_bss_color") == 0) {
 		conf->he_op.he_bss_color = atoi(pos) & 0x3f;
 		conf->he_op.he_bss_color_disabled = 0;
+		wpa_printf(MSG_ERROR, "Line %d: set he_bss_color: %d",
+			   line, conf->he_op.he_bss_color);
 	} else if (os_strcmp(buf, "he_bss_color_partial") == 0) {
 		conf->he_op.he_bss_color_partial = atoi(pos);
 	} else if (os_strcmp(buf, "he_default_pe_duration") == 0) {
