b65f08c25862cc72ba96a2009b5c204d362b626c
StanleyYP Wang <StanleyYP.Wang@mediatek.com>
2024-06-28 14:05:21 +0800

    mtk: hostapd: add support for link add
    Add support for adding affiliated APs to AP MLD via the following link add command
    hostapd_cli -i <mld intf> raw LINK_ADD bss_config=phyX:<bss_config>
    <bss_config> is per-bss config
    
    Add another cmd format:
    hostapd_cli -i <mld intf> link_add bss_config=phyX:<bss_config>
    
    Current link removal is implemented by hostapd_disable_iface for
    the sake of simplicity.
    When a link is added back after removal, hostapd_enable_iface should be
    called instead of hostapd_add_iface.
    Therefore, this patch is added to support iface enablement in link add cmd
    as a temporary workaround.
    
    Avoid modifying the original cmd str before entering hostapd_add_iface.
    Otherwise, hostapd_add_iface will fail to parse the cmd str correctly.
    
    Signed-off-by: StanleyYP Wang <StanleyYP.Wang@mediatek.com>

--- src/ap/beacon.c
+++ src/ap/beacon.c
@@ -2791,6 +2791,14 @@ static int __ieee802_11_set_beacon(struct hostapd_data *hapd)
 		return 0;
 	}
 
+#ifdef CONFIG_IEEE80211BE
+#ifdef CONFIG_TESTING_OPTIONS
+	if (hapd->conf->mld_ap && hapd->mld &&
+	    (hapd->mld->link_reconf_in_progress & BIT(hapd->mld_link_id)))
+		ieee802_11_set_bss_critical_update(hapd, BSS_CRIT_UPDATE_EVENT_ADD_LINK);
+#endif /* CONFIG_TESTING_OPTIONS */
+#endif /* CONFIG_IEEE80211BE */
+
 	hapd->beacon_set_done = 1;
 
 	if (ieee802_11_build_ap_params(hapd, &params) < 0)
@@ -3392,11 +3400,17 @@ int ieee802_11_set_beacon(struct hostapd_data *hapd)
 	for_each_mld_link(link_bss, hapd) {
 		hostapd_gen_per_sta_profiles(link_bss);
 
-		/* clear critical update flag for UPDATE_SINGLE type, for other types,
-		 * we should get some notified events from driver
+		/* clear critical update flag for UPDATE_SINGLE type & link adding,
+		 * for other types, we should get some notified events from driver
 		 */
+		/* TODO:  Check final upstream code for 'h->' */
 		if (hapd->eht_mld_bss_critical_update == BSS_CRIT_UPDATE_SINGLE)
 			hapd->eht_mld_bss_critical_update = 0;
+		if (hapd->eht_mld_bss_critical_update == BSS_CRIT_UPDATE_FLAG &&
+		    (hapd->mld->link_reconf_in_progress & BIT(hapd->mld_link_id))) {
+			hapd->mld->link_reconf_in_progress &= ~BIT(hapd->mld_link_id);
+			hapd->eht_mld_bss_critical_update = 0;
+		}
 	}
 #endif /* CONFIG_IEEE80211BE */
 
