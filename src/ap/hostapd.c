e8b91f3e08024a5dfff5d0ad9f98fcf71658e55d
Bo Jiao <Bo.Jiao@mediatek.com>
2024-07-10 14:49:43 +0800

    mtk: hostapd: fix Multiple MLDs to use the conf's own_addr/mld_addr.
    Signed-off-by: Bo Jiao <Bo.Jiao@mediatek.com>

--- src/ap/hostapd.c
+++ src/ap/hostapd.c
@@ -1529,6 +1529,9 @@ static int hostapd_setup_bss(struct hostapd_data *hapd, int first,
 					   hapd->mld_link_id, hapd->conf->iface);
 				goto setup_mld;
 			}
+
+			if (addr && !is_zero_ether_addr(hapd->conf->mld_addr))
+				os_memcpy(addr, hapd->conf->mld_addr, ETH_ALEN);
 		}
 #endif /* CONFIG_IEEE80211BE */
 
@@ -1554,6 +1557,9 @@ static int hostapd_setup_bss(struct hostapd_data *hapd, int first,
 				   hapd->mld_link_id, hapd->conf->iface);
 			os_memcpy(hapd->mld->mld_addr, hapd->own_addr,
 				  ETH_ALEN);
+
+			if (!is_zero_ether_addr(conf->bssid))
+				os_memcpy(hapd->own_addr, conf->bssid, ETH_ALEN);
 		}
 #endif /* CONFIG_IEEE80211BE */
 	}
