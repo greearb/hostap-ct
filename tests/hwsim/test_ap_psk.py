e0053bf9d0bfd741d99245235c2672f6afd8ec45
Jouni Malinen <quic_jouni@quicinc.com>
2024-07-24 20:35:11 +0000

    tests: Long extra KDE in EAPOL-Key msg 3/4
    Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

--- tests/hwsim/test_ap_psk.py
+++ tests/hwsim/test_ap_psk.py
@@ -3759,6 +3759,21 @@ def test_rsn_eapol_m3_extra(dev, apdev):
 
     dev[0].connect(ssid, psk=passphrase, scan_freq="2412")
 
+def test_rsn_eapol_m3_extra_long(dev, apdev):
+    """Long extra KDE in EAPOL-Key msg 3/4"""
+    ssid = "test-rsn"
+    passphrase = 'qwertyuiop'
+    params = hostapd.wpa2_params(ssid=ssid, passphrase=passphrase)
+    # Add a reserved KDEs into EAPOL-Key msg 3/4
+    val = 'dd0507c0d19311'
+    val += 'ddff69b847070102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafb'
+    val += 'dd085ba59d7911223344'
+    val += 'dd0a000face4112233445566'
+    params['eapol_m3_elements'] = val
+    hapd = hostapd.add_ap(apdev[0], params)
+
+    dev[0].connect(ssid, psk=passphrase, scan_freq="2412")
+
 def test_rsn_eapol_m3_no_encrypt(dev, apdev):
     """EAPOL-Key msg 3/4 Key Data field not encrypted"""
     ssid = "test-rsn"
