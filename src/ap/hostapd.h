b240494892e446ceacab4d83ef0bc323f7ff14b0
MeiChia Chiu <MeiChia.Chiu@mediatek.com>
2024-09-06 17:22:37 +0800

    mtk: hostapd: Add support for TSF offset
    To add support for handling TSF offset event, and
    populate the TSF offset in the per-station profile
    within the beacon
    
    Signed-off-by: MeiChia Chiu <MeiChia.Chiu@mediatek.com>

--- src/ap/hostapd.h
+++ src/ap/hostapd.h
@@ -510,6 +510,8 @@ struct hostapd_data {
 	 * total, additional 7 characters required. */
 	char ctrl_sock_iface[IFNAMSIZ + 7 + 1];
 
+	s64 tsf_offset[MAX_NUM_MLD_LINKS];
+
 #ifdef CONFIG_TESTING_OPTIONS
 	u8 eht_mld_link_removal_count;
 #endif /* CONFIG_TESTING_OPTIONS */
@@ -889,7 +891,7 @@ int hostapd_fill_cca_settings(struct hostapd_data *hapd,
 #ifdef CONFIG_IEEE80211BE
 
 void hostapd_event_attlm(struct hostapd_data *hapd, struct attlm_event *attlm_event);
-
+void hostapd_event_tsf_offset(struct hostapd_data *hapd, s64 *tsf_offset);
 bool hostapd_mld_is_first_bss(struct hostapd_data *hapd);
 void hostapd_mld_interface_freed(struct hostapd_data *hapd);
 
