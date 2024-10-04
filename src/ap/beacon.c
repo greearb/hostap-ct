411e02a4d03591b2b8ef7a71f78e29db1d095f4c
Michael-CY Lee <michael-cy.lee@mediatek.com>
2024-10-04 08:35:54 +0800

    mtk: hostapd: add return value for ieee802_11_set_beacon_per_bss_only
    Since __ieee802_11_set_beacon() can fail, the
    ieee802_11_set_beacon_per_bss_only should forward the return value to
    its caller.
    
    Signed-off-by: Michael-CY Lee <michael-cy.lee@mediatek.com>

--- src/ap/beacon.c
+++ src/ap/beacon.c
@@ -2858,9 +2858,9 @@ fail:
 }
 
 
-void ieee802_11_set_beacon_per_bss_only(struct hostapd_data *hapd)
+int ieee802_11_set_beacon_per_bss_only(struct hostapd_data *hapd)
 {
-	__ieee802_11_set_beacon(hapd);
+	return __ieee802_11_set_beacon(hapd);
 }
 
 
