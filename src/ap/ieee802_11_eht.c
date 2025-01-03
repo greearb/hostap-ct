39698d222b7bc66428f8f40abc6d28ad52eda153
Michael-CY Lee <michael-cy.lee@mediatek.com>
2025-01-03 12:01:07 +0800

    mtk: hostapd: unset MLE cap bit 3 for Wi-Fi 7 CERT 4.50
    A new configuration option "neg_ttlm_support_mode" is added for the
    Wi-Fi 7 CERT 4.50, which requires TID-To-Link Mapping Negotiation
    Support subfield in MLD Capabilities And Operations field being 1
    
    Signed-off-by: Michael-CY Lee <michael-cy.lee@mediatek.com>

--- src/ap/ieee802_11_eht.c
+++ src/ap/ieee802_11_eht.c
@@ -602,6 +602,10 @@ u8 * hostapd_eid_eht_basic_ml_common(struct hostapd_data *hapd,
 	/* TODO: Advertise T2LM based on driver support as well */
 	mld_cap |= EHT_ML_MLD_CAPA_TID_TO_LINK_MAP_ALL_TO_ALL;
 
+	/* For CERT wifi7 4.50 */
+	if (hapd->iconf->neg_ttlm_support_mode == 1)
+		mld_cap &= ~EHT_ML_MLD_CAPA_TID_TO_LINK_MAP_ALL_TO_ONE;
+
 	wpa_printf(MSG_DEBUG, "MLD: MLD Capabilities and Operations=0x%x",
 		   mld_cap);
 	wpabuf_put_le16(buf, mld_cap);
