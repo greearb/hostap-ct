e4490d4dad76d522ae8a3d2292685b9ecbc0552b
Allen Ye <allen.ye@mediatek.com>
2025-01-16 18:08:33 +0800

    mtk: hostapd: FT: Add AP FT-Assoc mld support
    Add AP ftie mlo groups support and add fragmentation of ftie in case
    multi-links may cause the length of ftie larger than 255.
    
    Signed-off-by: Allen Ye <allen.ye@mediatek.com>

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -5278,7 +5278,8 @@ static u16 send_assoc_resp(struct hostapd_data *hapd, struct sta_info *sta,
 		p = wpa_sm_write_assoc_resp_ies(sta->wpa_sm, p,
 						buf + buflen - p,
 						sta->auth_alg, ies, ies_len,
-						omit_rsnxe);
+						omit_rsnxe,
+						ap_sta_is_mld(hapd, sta));
 		if (!p) {
 			wpa_printf(MSG_DEBUG,
 				   "FT: Failed to write AssocResp IEs");
