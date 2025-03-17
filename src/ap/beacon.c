6dbed1a21d7e3ab2148271aa48d3d67d8d837575
Michael-CY Lee <michael-cy.lee@mediatek.com>
2025-03-17 13:56:31 +0800

    mtk: hostapd: add hostapd_eid_wmm_len()
    Signed-off-by: Michael-CY Lee <michael-cy.lee@mediatek.com>

--- src/ap/beacon.c
+++ src/ap/beacon.c
@@ -823,6 +823,7 @@ static size_t hostapd_probe_resp_elems_len(struct hostapd_data *hapd,
 	}
 #endif /* CONFIG_IEEE80211BN */
 
+	buflen += hostapd_eid_wmm_len(hapd);
 	buflen += hostapd_mbo_ie_len(hapd);
 	buflen += hostapd_eid_owe_trans_len(hapd);
 	buflen += hostapd_eid_dpp_cc_len(hapd);
@@ -2334,6 +2335,7 @@ int ieee802_11_build_ap_params(struct hostapd_data *hapd,
 		tail_len += 3 + sizeof(struct ieee80211_uhr_operation);
 #endif /* CONFIG_IEEE80211BN */
 
+	tail_len += hostapd_eid_wmm_len(hapd);
 	tail_len += hostapd_mbo_ie_len(hapd);
 	tail_len += hostapd_eid_owe_trans_len(hapd);
 	tail_len += hostapd_eid_dpp_cc_len(hapd);
