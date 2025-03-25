6a97d18cf0474ac4ee9192a2ecfeecc7dd14eebf
Allen Ye <allen.ye@mediatek.com>
2025-03-25 11:39:42 +0800

    mtk: hostapd: FT: Use primary wpa_auth to handle rrb in ml sta connect
    Fix target FTR may fail to receive PMK-R1 when enable mld.
    Use primary wpa_auth handle FT rrb frames to prevent that pnk push and rrb
    sequence responses from different link. Which will make target FTR set invalid
    rrb sequence.
    Once ap receive a pmk push successfully, copy the pmk to each mld link.
    
    When current FTR send a PMK-R1 push or pull response to a target FTR. The
    FTR may send sequence request frame to current FTR. If current FTR is a
    mld ap, each link will response their rrb sequence. The sequence number from
    non-assoc link is invalid to PMK-R1 push or pull frame. That may make target
    FTR failed to store PMK-R1.
    
    Signed-off-by: Allen Ye <allen.ye@mediatek.com>

--- src/ap/wpa_auth.h
+++ src/ap/wpa_auth.h
@@ -712,4 +712,6 @@ bool wpa_auth_sm_known_sta_identification(struct wpa_state_machine *sm,
 
 const u8 * wpa_auth_get_aa(const struct wpa_state_machine *sm);
 const u8 * wpa_auth_get_spa(const struct wpa_state_machine *sm);
+struct wpa_authenticator * wpa_get_link_auth(struct wpa_authenticator *wpa_auth,
+					     int link_id);
 #endif /* WPA_AUTH_H */
