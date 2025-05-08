8551a4a1406ae829beb68b763ac23645d5acd1ce
Shayne Chen <shayne.chen@mediatek.com>
2025-05-08 18:41:23 +0800

    mtk: hostapd: fix compile error when TESTING_OPTION is not set
    Signed-off-by: Shayne Chen <shayne.chen@mediatek.com>

--- src/ap/hostapd.h
+++ src/ap/hostapd.h
@@ -905,8 +905,11 @@ int hostapd_fill_cca_settings(struct hostapd_data *hapd,
 			      struct cca_settings *settings);
 
 #ifdef CONFIG_IEEE80211BE
-
 bool hostapd_is_attlm_active(struct hostapd_data *hapd);
+#ifdef CONFIG_TESTING_OPTIONS
+void hostapd_link_remove_cb(struct hostapd_data *hapd, u16 removed_links);
+int hostapd_mld_set_attlm(struct hostapd_data *hapd);
+#endif /* CONFIG_TESTING_OPTIONS */
 void hostapd_event_attlm(struct hostapd_data *hapd, struct attlm_event *attlm_event);
 void hostapd_event_tsf_offset(struct hostapd_data *hapd, s64 *tsf_offset);
 bool hostapd_mld_is_first_bss(struct hostapd_data *hapd);
