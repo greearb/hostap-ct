ce5caf71dbec0b8b21319376006e7026c1c1db66
Dylan Eskew <dylan.eskew@candelatech.com>
2025-06-04 14:59:08 -0700

    supplicant: Support PMKID corruption
    Allow user to explicity set a static PMKID ID or to
    occasionally twiddle PMKID bits when reasociating.

--- wpa_supplicant/config.h
+++ wpa_supplicant/config.h
@@ -1176,6 +1176,8 @@ struct wpa_config {
 	unsigned short delay_eapol_id_req_max;
 	unsigned short delay_eapol_other_req_min;
 	unsigned short delay_eapol_other_req_max;
+
+	unsigned short corrupt_pmkid;
 #endif
 
 	/**
