8506affcdfe3ec94d94a342a4be627792c6b467a
Muna Sinada <muna.sinada@oss.qualcomm.com>
2025-06-09 11:06:15 -0700

    AP MLD: Use MLD MAC address for creating new VLAN interfaces
    Signed-off-by: Muna Sinada <muna.sinada@oss.qualcomm.com>
    Signed-off-by: Pradeep Kumar Chitrapu <quic_pradeepc@quicinc.com>

--- src/ap/ap_drv_ops.c
+++ src/ap/ap_drv_ops.c
@@ -370,7 +370,14 @@ int hostapd_vlan_if_add(struct hostapd_data *hapd, const char *ifname)
 {
 	char force_ifname[IFNAMSIZ];
 	u8 if_addr[ETH_ALEN];
-	return hostapd_if_add(hapd, WPA_IF_AP_VLAN, ifname, hapd->own_addr,
+	const u8 *addr = hapd->own_addr;
+
+#ifdef CONFIG_IEEE80211BE
+	if (hapd->conf->mld_ap)
+		addr = hapd->mld->mld_addr;
+#endif /* CONFIG_IEEE80211BE */
+
+	return hostapd_if_add(hapd, WPA_IF_AP_VLAN, ifname, addr,
 			      NULL, NULL, force_ifname, if_addr, NULL, 0);
 }
 
