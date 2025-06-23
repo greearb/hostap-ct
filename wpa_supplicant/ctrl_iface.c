greearb/hostap-ct

commit e0935844724a8a5e03fe2a4f9dcaab5b962ea7eb

STA: Update driver roaming policy on connection completion
When the network profile is configured with BSSID before connection,
roaming policy in the driver (for driver-based BSS selection) doesn't
get updated if the same BSSID is configured after connection. Update
roaming policy to the driver on connection completion to cover this
case.

Signed-off-by: Purushottam Kushwaha <quic_pkushwah@quicinc.com>

wpa_supplicant/notify.c
@@ -88,6 +88,8 @@ void wpas_notify_state_changed(struct wpa_supplicant *wpa_s,
 			       enum wpa_states new_state,
 			       enum wpa_states old_state)
 {
+	struct wpa_ssid *ssid = wpa_s->current_ssid;
+
 	if (wpa_s->p2p_mgmt)
 		return;
 
@@ -104,10 +106,14 @@ void wpas_notify_state_changed(struct wpa_supplicant *wpa_s,
 	}
 #endif /* CONFIG_FST */
 
-	if (new_state == WPA_COMPLETED)
+	if (new_state == WPA_COMPLETED) {
 		wpas_p2p_notif_connected(wpa_s);
-	else if (old_state >= WPA_ASSOCIATED && new_state < WPA_ASSOCIATED)
+		if (ssid)
+			wpa_drv_roaming(wpa_s, !ssid->bssid_set,
+					ssid->bssid_set ? ssid->bssid : NULL);
+	} else if (old_state >= WPA_ASSOCIATED && new_state < WPA_ASSOCIATED) {
 		wpas_p2p_notif_disconnected(wpa_s);
+	}
 
 	sme_state_changed(wpa_s);
 

commit 96f5ab644f2437cc2e9be1d26b2635165aea9461

Preserve user roaming configuration across connection state changes
Prevent unintended roaming re-enablement after connection state
changes, even when user configuration disables it.

If user disables roaming, but after each state change post-connection,
wpa_supplicant re-enables roaming when bssid_set is false. This migh
cause the driver to send a roam enable configuration to the firmware,
overriding the user's intent.

Fix this by ensuring wpa_supplicant logic to check both bssid_set and
the user’s roaming configuration (`SET roaming`) after each connection
state change. Ensure roaming is only re-enabled if explicitly
configured, allowing the driver to maintain the intended roaming state
throughout the connection.

Fixes: e0935844724a ("STA: Update driver roaming policy on connection completion")
Signed-off-by: Shubham Joshi <shujosh@qti.qualcomm.com>

wpa_supplicant/ctrl_iface.c
@@ -964,6 +964,7 @@ static int wpa_supplicant_ctrl_iface_set(struct wpa_supplicant *wpa_s,
 	} else if (os_strcasecmp(cmd, "ric_ies") == 0) {
 		ret = wpas_ctrl_iface_set_ric_ies(wpa_s, value);
 	} else if (os_strcasecmp(cmd, "roaming") == 0) {
+		wpa_s->sta_roaming_disabled = atoi(value) ? false : true;
 		ret = wpa_drv_roaming(wpa_s, atoi(value), NULL);
 #ifdef CONFIG_WNM
 	} else if (os_strcasecmp(cmd, "coloc_intf_elems") == 0) {

wpa_supplicant/notify.c
@@ -108,7 +108,7 @@ void wpas_notify_state_changed(struct wpa_supplicant *wpa_s,
 
 	if (new_state == WPA_COMPLETED) {
 		wpas_p2p_notif_connected(wpa_s);
-		if (ssid)
+		if (ssid && !wpa_s->sta_roaming_disabled)
 			wpa_drv_roaming(wpa_s, !ssid->bssid_set,
 					ssid->bssid_set ? ssid->bssid : NULL);
 	} else if (old_state >= WPA_ASSOCIATED && new_state < WPA_ASSOCIATED) {

wpa_supplicant/wpa_supplicant_i.h
@@ -744,6 +744,7 @@ struct wpa_supplicant {
 	} links[MAX_NUM_MLD_LINKS];
 	u8 *last_con_fail_realm;
 	size_t last_con_fail_realm_len;
+	bool sta_roaming_disabled;
 
 	/* Selected configuration (based on Beacon/ProbeResp WPA IE) */
 	int pairwise_cipher;