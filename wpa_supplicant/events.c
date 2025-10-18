greearb/hostap-ct

commit a58a0c592e20978b834291f15dcfeba1ef49332f

MLD: Fix Multi-Link element parsing for association failures
The Common Info and STA Info fields are supposed to be extensible and as
such, their length fields need to be verified to be large enough, but if
there are unknown extra fields after the known fields, those need to be
silently ignored instead of rejecting the element.

Fixes: 5af986c75af4 ("MLD: Also mark links as failed after association failure")
Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

wpa_supplicant/events.c
@@ -3991,7 +3991,7 @@ static unsigned int wpas_ml_parse_assoc(struct wpa_supplicant *wpa_s,
 	}
 
 	common_info = (struct eht_ml_basic_common_info *) ml->variable;
-	if (common_info->len != expected_common_info_len) {
+	if (common_info->len < expected_common_info_len) {
 		wpa_printf(MSG_DEBUG,
 			   "MLD: Invalid common info len=%u. expected=%u",
 			   common_info->len, expected_common_info_len);
@@ -4132,12 +4132,13 @@ static unsigned int wpas_ml_parse_assoc(struct wpa_supplicant *wpa_s,
 		sta_info_len = 1 + ETH_ALEN + 8 + 2 + 2 + 1 + nstr_bitmap_len;
 		if (sta_info_len > ml_len || sta_info_len > end - pos ||
 		    sta_info_len + 2 > sub_elem_len ||
-		    sta_info_len != *pos) {
+		    sta_info_len > *pos) {
 			wpa_printf(MSG_DEBUG,
 				   "MLD: Invalid STA info len=%u, len=%u",
 				   sta_info_len, *pos);
 			goto out;
 		}
+		sta_info_len = *pos;
 
 		/* Get the link address */
 		wpa_printf(MSG_DEBUG,

commit 4c93878208d9dfb838685c4b1b8f7ea8906abe43

MLD: Fix MLE STA Info length check in association failure cases
The checks for this field were not complete when the earlier strict
length enforcement was removed. Allow flexibility for a longer field
while still validating the value properly.

Fixes: a58a0c592e20 ("MLD: Fix Multi-Link element parsing for association failures")
Signed-off-by: Jouni Malinen <j@w1.fi>

wpa_supplicant/events.c
@@ -4133,18 +4133,21 @@ static unsigned int wpas_ml_parse_assoc(struct wpa_supplicant *wpa_s,
 			nstr_bitmap_len;
 		if (sta_info_len_min > ml_len ||
 		    sta_info_len_min > (size_t) (end - pos) ||
-		    sta_info_len_min + 2 > sub_elem_len ||
-		    sta_info_len_min > *pos) {
+		    sta_info_len_min + 2 > sub_elem_len) {
 			wpa_printf(MSG_DEBUG,
 				   "MLD: Invalid STA info min len=%zu, len=%u",
 				   sta_info_len_min, *pos);
 			goto out;
 		}
 		sta_info_len = *pos;
-		/* Make static analyzers happier with an explicit check even
-		 * though this was already checked above with *pos.. */
-		if (sta_info_len < sta_info_len_min)
+		if (sta_info_len > ml_len ||
+		    sta_info_len > sub_elem_len - 2 ||
+		    sta_info_len < sta_info_len_min) {
+			wpa_printf(MSG_DEBUG,
+				   "MLD: Invalid STA info min len=%zu, len=%zu",
+				   sta_info_len_min, sta_info_len);
 			goto out;
+		}
 
 		/* Get the link address */
 		wpa_printf(MSG_DEBUG,
@@ -4169,6 +4172,8 @@ static unsigned int wpas_ml_parse_assoc(struct wpa_supplicant *wpa_s,
 		wpa_hexdump(MSG_MSGDUMP, "MLD: STA profile", pos, sub_elem_len);
 		ml_info[i].status = WPA_GET_LE16(pos + 2);
 
+		if (sub_elem_len > ml_len)
+			goto out;
 		pos += sub_elem_len;
 		ml_len -= sub_elem_len;
 