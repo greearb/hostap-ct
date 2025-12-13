greearb/hostap-ct

commit 0a76c7ed64de46eb9019cf18906ce821941020eb

wpa_debug: Prefer tracefs over debugfs
With Linux 6.17, having /sys/kernel/debug/tracing/ is now optional
(CONFIG_TRACEFS_AUTOMOUNT_DEPRECATED), since tracefs has been around for
a long time. Prefer tracefs when finding the trace_marker file.

Note the "!buf[0]" condition which serves to keep the logic identical to
before when tracefs isn't present, using the first debugfs rather than
the last. That is not likely to matter, but still.

Also mount tracefs in the scripts for running in VMs.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>

src/utils/wpa_debug.c
@@ -130,7 +130,7 @@ int wpa_debug_open_linux_tracing(void)
 	int mounts, trace_fd;
 	char buf[4096] = {};
 	ssize_t buflen;
-	char *line, *tmp1, *path = NULL;
+	char *line, *tmp1;
 
 	mounts = open("/proc/mounts", O_RDONLY);
 	if (mounts < 0) {
@@ -153,20 +153,31 @@ int wpa_debug_open_linux_tracing(void)
 		strtok_r(line, " ", &tmp2);
 		tmp_path = strtok_r(NULL, " ", &tmp2);
 		fstype = strtok_r(NULL, " ", &tmp2);
-		if (fstype && strcmp(fstype, "debugfs") == 0) {
-			path = tmp_path;
+
+		if (!buf[0] && fstype && os_strcmp(fstype, "debugfs") == 0) {
+			os_snprintf(buf, sizeof(buf) - 1,
+				    "%s/tracing/trace_marker",
+				    tmp_path);
+			/*
+			 * Don't break to prefer tracefs, which if present may
+			 * mean debugfs doesn't have tracing/ (depending on the
+			 * kernel version).
+			 */
+		}
+
+		if (fstype && os_strcmp(fstype, "tracefs") == 0) {
+			os_snprintf(buf, sizeof(buf) - 1, "%s/trace_marker",
+				    tmp_path);
 			break;
 		}
 
 		line = strtok_r(NULL, "\n", &tmp1);
 	}
 
-	if (path == NULL) {
-		printf("debugfs mountpoint not found\n");
+	if (!buf[0]) {
+		printf("tracefs/debugfs not found\n");
 		return -1;
 	}
-
-	snprintf(buf, sizeof(buf) - 1, "%s/tracing/trace_marker", path);
 
 	trace_fd = open(buf, O_WRONLY);
 	if (trace_fd < 0) {

tests/hwsim/vm/inside.sh
@@ -14,6 +14,7 @@ mount proc -t proc /proc
 mount sysfs -t sysfs /sys
 # needed for tracing
 mount debugfs -t debugfs /sys/kernel/debug
+mount tracefs -t tracefs /sys/kernel/tracing
 
 mkdir /tmp/wireshark-share
 mount --bind /usr/share/wireshark /tmp/wireshark-share

commit 672da5df77220684b9bbf3de5f5411c42a1e6380

wpa_debug: Use separate buffer for path and improve error checking
Using the same buffer for output and input could already result in
an overlapping source and destination in snprintf. This was working
fine, however, with the patch to also find tracefs, we continue to parse
the buffer. In that case, the continued parsing can corrupt the found
path causing an error.

Fix this problem and reshuffle the code a bit to make it a bit more
clear and improve the condition to skip lines that could not be parsed
properly.

Fixes: 0a76c7ed64de ("wpa_debug: Prefer tracefs over debugfs")
Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>

src/utils/wpa_debug.c
@@ -128,6 +128,7 @@ static int syslog_priority(int level)
 int wpa_debug_open_linux_tracing(void)
 {
 	int mounts, trace_fd;
+	char path[128] = {};
 	char buf[4096] = {};
 	ssize_t buflen;
 	char *line, *tmp1;
@@ -146,16 +147,20 @@ int wpa_debug_open_linux_tracing(void)
 	}
 	buf[buflen] = '\0';
 
-	line = strtok_r(buf, "\n", &tmp1);
-	while (line) {
+	for (line = strtok_r(buf, "\n", &tmp1);
+	     line;
+	     line = strtok_r(NULL, "\n", &tmp1)) {
 		char *tmp2, *tmp_path, *fstype;
 		/* "<dev> <mountpoint> <fs type> ..." */
 		strtok_r(line, " ", &tmp2);
 		tmp_path = strtok_r(NULL, " ", &tmp2);
 		fstype = strtok_r(NULL, " ", &tmp2);
 
-		if (!buf[0] && fstype && os_strcmp(fstype, "debugfs") == 0) {
-			os_snprintf(buf, sizeof(buf) - 1,
+		if (!line[0] || !tmp_path || !fstype)
+			continue;
+
+		if (os_strcmp(fstype, "debugfs") == 0) {
+			os_snprintf(path, sizeof(path) - 1,
 				    "%s/tracing/trace_marker",
 				    tmp_path);
 			/*
@@ -165,21 +170,19 @@ int wpa_debug_open_linux_tracing(void)
 			 */
 		}
 
-		if (fstype && os_strcmp(fstype, "tracefs") == 0) {
-			os_snprintf(buf, sizeof(buf) - 1, "%s/trace_marker",
+		if (os_strcmp(fstype, "tracefs") == 0) {
+			os_snprintf(path, sizeof(path) - 1, "%s/trace_marker",
 				    tmp_path);
 			break;
 		}
-
-		line = strtok_r(NULL, "\n", &tmp1);
 	}
 
-	if (!buf[0]) {
+	if (!path[0]) {
 		printf("tracefs/debugfs not found\n");
 		return -1;
 	}
 
-	trace_fd = open(buf, O_WRONLY);
+	trace_fd = open(path, O_WRONLY);
 	if (trace_fd < 0) {
 		printf("failed to open trace_marker file\n");
 		return -1;