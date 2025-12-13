greearb/hostap-ct

commit e166a37632d45fa93bf0f0102c5bab37062e898c

AP MLD: Control interface events for ML Setup Link Reconfiguration
Add control interface events in hostapd for link STA removal and
addition operations from link reconfiguration.

Co-developed-by: Pooventhiran G <quic_pooventh@quicinc.com>
Signed-off-by: Pooventhiran G <quic_pooventh@quicinc.com>
Signed-off-by: Mohan Kumar G <quic_mkumarg@quicinc.com>

src/ap/ieee802_11_eht.c
@@ -9,6 +9,7 @@
 #include "utils/includes.h"
 #include "utils/common.h"
 #include "common/ocv.h"
+#include "common/wpa_ctrl.h"
 #include "crypto/crypto.h"
 #include "crypto/dh_groups.h"
 #include "hostapd.h"
@@ -1684,6 +1685,9 @@ void hostapd_link_reconf_resp_tx_status(struct hostapd_data *hapd,
 
 		/* Free as a link STA */
 		ap_free_sta(lhapd, lsta);
+		wpa_msg(hapd->msg_ctx, MSG_INFO,
+			WPA_EVENT_LINK_STA_REMOVED "sta=" MACSTR " link_id=%u",
+			MAC2STR(lsta->addr), link_id);
 
 		for_each_mld_link(other_hapd, lhapd) {
 			struct mld_link_info *link;
@@ -2497,6 +2501,10 @@ hostapd_validate_link_reconf_req(struct hostapd_data *hapd,
 		} else {
 			total_kde_len += link_kde_len;
 			links_add_ok |= BIT(info->link_id);
+			wpa_msg(hapd->msg_ctx, MSG_INFO,
+				WPA_EVENT_LINK_STA_ADDED "sta=" MACSTR
+				" link_id=%u", MAC2STR(req_list->sta_mld_addr),
+				info->link_id);
 		}
 
 		info->status = status;

src/common/wpa_ctrl.h
@@ -118,6 +118,10 @@ extern "C" {
 #define WPA_EVENT_T2LM_UPDATE "CTRL-EVENT-T2LM-UPDATE "
 /** MLO link reconfiguration event */
 #define WPA_EVENT_LINK_RECONFIG "CTRL-EVENT-LINK-RECONFIG "
+/** MLO link STA removed through link reconfiguration */
+#define WPA_EVENT_LINK_STA_REMOVED "CTRL-EVENT-LINK-STA-REMOVED "
+/** MLO link STA added through link reconfiguration */
+#define WPA_EVENT_LINK_STA_ADDED "CTRL-EVENT-LINK-STA-ADDED "
 
 /** IP subnet status change notification
  *

commit 2dc279c1ef89332bdfd3de2bec1ed0f5340097e7

AP: Fix use after free in link reconfiguration
Print the station MAC address before freeing the data.

Fixes: e166a37632d4 ("AP MLD: Control interface events for ML Setup Link Reconfiguration)
CC: Mohan Kumar G <quic_mkumarg@quicinc.com>
Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>

src/ap/ieee802_11_eht.c
@@ -1677,10 +1677,10 @@ void hostapd_link_reconf_resp_tx_status(struct hostapd_data *hapd,
 		}
 
 		/* Free as a link STA */
-		ap_free_sta(lhapd, lsta);
 		wpa_msg(hapd->msg_ctx, MSG_INFO,
 			WPA_EVENT_LINK_STA_REMOVED "sta=" MACSTR " link_id=%u",
 			MAC2STR(lsta->addr), link_id);
+		ap_free_sta(lhapd, lsta);
 
 		for_each_mld_link(other_hapd, lhapd) {
 			struct mld_link_info *link;