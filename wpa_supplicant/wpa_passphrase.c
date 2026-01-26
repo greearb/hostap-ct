vanhoefm/macstealer

commit 5102d7411f0115f35558c2d836e26303eb2d205b

wpa_passphrase: Disable terminal echo when reading from stdin
Disable terminal echo using tcgetattr() and tcsetattr() when reading a
passphrase from stdin.

Signed-off-by: Abhiram V <abhi.raa.man.v@gmail.com>

wpa_supplicant/wpa_passphrase.c
@@ -7,6 +7,7 @@
  */
 
 #include "includes.h"
+#include <termios.h>
 
 #include "common.h"
 #include "crypto/sha1.h"
@@ -14,6 +15,7 @@
 
 int main(int argc, char *argv[])
 {
+	struct termios term;
 	unsigned char psk[32];
 	int i;
 	char *ssid, *passphrase, buf[64], *pos;
@@ -31,9 +33,26 @@ int main(int argc, char *argv[])
 	if (argc > 2) {
 		passphrase = argv[2];
 	} else {
+		bool ctrl_echo;
+
 		fprintf(stderr, "# reading passphrase from stdin\n");
+		if (tcgetattr(STDIN_FILENO, &term) < 0) {
+			perror("tcgetattr");
+			return 1;
+		}
+		ctrl_echo = term.c_lflag & ECHO;
+		term.c_lflag &= ~ECHO;
+		if (ctrl_echo && tcsetattr(STDIN_FILENO, TCSANOW, &term) < 0) {
+			perror("tcsetattr:error disabling echo");
+			return 1;
+		}
 		if (fgets(buf, sizeof(buf), stdin) == NULL) {
 			fprintf(stderr, "Failed to read passphrase\n");
+			return 1;
+		}
+		term.c_lflag |= ECHO;
+		if (ctrl_echo && tcsetattr(STDIN_FILENO, TCSANOW, &term) < 0) {
+			perror("tcsetattr:error enabling echo");
 			return 1;
 		}
 		buf[sizeof(buf) - 1] = '\0';

greearb/hostap-ct

commit e73a8f30868ea36f0f2159c95d9e3d9412d3b821

wpa_passphrase: Fix reading password without TTY
Terminal echo disabling ended up breaking the cases where a password is
read from a file redirection or pipe. Fix this by skipping that change
if there is no TTY.

Fixes: 5102d7411f01 ("wpa_passphrase: Disable terminal echo when reading from stdin")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/wpa_passphrase.c
@@ -33,10 +33,14 @@ int main(int argc, char *argv[])
 	if (argc > 2) {
 		passphrase = argv[2];
 	} else {
-		bool ctrl_echo;
+		bool ctrl_echo, notty = false;
 
 		fprintf(stderr, "# reading passphrase from stdin\n");
 		if (tcgetattr(STDIN_FILENO, &term) < 0) {
+			if (errno == ENOTTY) {
+				notty = true;
+				goto read_pw;
+			}
 			perror("tcgetattr");
 			return 1;
 		}
@@ -46,12 +50,14 @@ int main(int argc, char *argv[])
 			perror("tcsetattr:error disabling echo");
 			return 1;
 		}
+	read_pw:
 		if (fgets(buf, sizeof(buf), stdin) == NULL) {
 			fprintf(stderr, "Failed to read passphrase\n");
 			return 1;
 		}
 		term.c_lflag |= ECHO;
-		if (ctrl_echo && tcsetattr(STDIN_FILENO, TCSANOW, &term) < 0) {
+		if (!notty &&
+		    ctrl_echo && tcsetattr(STDIN_FILENO, TCSANOW, &term) < 0) {
 			perror("tcsetattr:error enabling echo");
 			return 1;
 		}