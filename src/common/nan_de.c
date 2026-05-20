mj22226/hostapd

commit f22db27253b22843305659299b139ba5a9e632fd

NAN: Indicate if a service supports pairing and/or NPK/NIK caching
Extend the NAN DE discovery result callback to also indicate support for
pairing and NPK/NIK caching. In addition, extend the discovery result
notification to include this information.

Signed-off-by: Ilan Peer <ilan.peer@intel.com>

src/common/nan_de.c
@@ -1293,6 +1293,32 @@ static void nan_de_process_elem_container(struct nan_de *de, const u8 *buf,
 }
 
 
+static void nan_de_parse_dcea(const u8 *buf, size_t len, bool *pairing_setup,
+			      bool *npk_nik_caching)
+{
+	const u8 *dcea;
+	u16 dcea_len;
+
+	*pairing_setup = false;
+	*npk_nik_caching = false;
+
+	dcea = nan_de_get_attr(buf, len, NAN_ATTR_DCEA, 0);
+	if (!dcea)
+		return;
+
+	dcea_len = WPA_GET_LE16(dcea + 1);
+	if (dcea_len < 2) {
+		wpa_printf(MSG_DEBUG, "NAN: DCEA length=%u too short",
+			   dcea_len);
+		return;
+	}
+
+	*pairing_setup =  !!(dcea[4] & NAN_DEV_CAPA_EXT_INFO_1_PAIRING_SETUP);
+	*npk_nik_caching = !!(dcea[4] &
+			      NAN_DEV_CAPA_EXT_INFO_1_NPK_NIK_CACHING);
+}
+
+
 static bool nan_de_filter_match(struct nan_de_service *srv,
 				const u8 *matching_filter,
 				size_t matching_filter_len)
@@ -1461,6 +1487,14 @@ send_event:
 		pmkid_count = nan_de_parse_scia(buf, buf_len, instance_id,
 						pmkid_list,
 						sizeof(pmkid_list) / PMKID_LEN);
+
+		/*
+		 * Parse Device Capability Extension attribute for pairing
+		 * setup and NPK/NIK caching support
+		 */
+		nan_de_parse_dcea(buf, buf_len,
+				  &res.pairing_setup_supp,
+				  &res.npk_nik_caching_supp);
 	}
 
 	os_memset(&res, 0, sizeof(res));

src/common/nan_de.h
@@ -37,6 +37,8 @@ struct nan_discovery_result {
 	unsigned int pmkid_count;
 	const u8 *cipher_suites;
 	unsigned int n_cipher_suites;
+	bool pairing_setup_supp;
+	bool npk_nik_caching_supp;
 };
 
 struct nan_callbacks {

wpa_supplicant/notify.c
@@ -1158,14 +1158,15 @@ err:
 
 	wpa_msg_global(wpa_s, MSG_INFO, NAN_DISCOVERY_RESULT
 		       "subscribe_id=%d publish_id=%d address=" MACSTR
-		       " fsd=%d fsd_gas=%d srv_proto_type=%u ssi=%s%s%s%s%s",
+		       " fsd=%d fsd_gas=%d srv_proto_type=%u ssi=%s%s%s%s%s pairing_setup_supp=%d npk_nik_caching_supp=%d",
 		       res->subscribe_id, res->peer_publish_id,
 		       MAC2STR(res->peer_addr),
 		       res->fsd, res->fsd_gas, res->srv_proto_type, ssi_hex,
 		       pmkid_hex ? " pmkid=" : "",
 		       pmkid_hex ? pmkid_hex : "",
 		       cipher_suites_str ? " cipher_suites=" : "",
-		       cipher_suites_str ? cipher_suites_str : "");
+		       cipher_suites_str ? cipher_suites_str : "",
+		       res->pairing_setup_supp, res->npk_nik_caching_supp);
 	os_free(ssi_hex);
 	os_free(pmkid_hex);
 	os_free(cipher_suites_str);

greearb/hostap-ct

commit 42ff0f03f8ba0b8998c9adc61714ec0afc6c2980

NAN: Fix setting PBM in NAN discovery event
The pairing and bootstrapping value was set before the NAN discovery
object was memset. Fix this by moving the memset() call earlier.

Fixes: f22db27253b2 ("NAN: Indicate if a service supports pairing and/or NPK/NIK caching")
Signed-off-by: Ilan Peer <ilan.peer@intel.com>

src/common/nan_de.c
@@ -1529,6 +1529,7 @@ static bool nan_de_rx_publish(struct nan_de *de, struct nan_de_service *srv,
 	}
 
 send_event:
+	os_memset(&res, 0, sizeof(res));
 	if (buf && buf_len > 0) {
 		/* Parse Cipher Suite Information Attribute */
 		cipher_suite_count = nan_de_parse_csia(
@@ -1552,7 +1553,6 @@ send_event:
 		res.pbm = nan_de_get_advertise_pbm(buf, buf_len);
 	}
 
-	os_memset(&res, 0, sizeof(res));
 	res.subscribe_id = srv->id;
 	res.srv_proto_type = srv_proto_type;
 	res.ssi = ssi;