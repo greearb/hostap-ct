mj22226/hostapd

commit e9a52e9d6adc6710e4c016428fb23dffb1eb29f2

EPPKE: EPPKE authentication for SME-in-driver case
Extend SME implementation to integrate EPPKE flow with the external
authentication framework and PASN initiator for building and parsing
Authentication frames in accordance with IEEE P802.11bi/D4.0, 12.16.9.

EPPKE authentication is enabled only when explicitly configured in the
network profile and supported by the driver and AP.

Signed-off-by: Ainy Kumari <ainy.kumari@oss.qualcomm.com>

wpa_supplicant/sme.c
@@ -2201,6 +2201,41 @@ static bool is_sae_key_mgmt_suite(struct wpa_supplicant *wpa_s, u32 suite)
 }
 
 
+#ifdef CONFIG_ENC_ASSOC
+static int sme_handle_eppke_external_auth_start(struct wpa_supplicant *wpa_s,
+						union wpa_event_data *data)
+{
+	u8 peer_addr[ETH_ALEN];
+	bool is_ml_peer;
+
+	if (data->external_auth.mld_addr) {
+		is_ml_peer = true;
+		os_memcpy(peer_addr, data->external_auth.mld_addr, ETH_ALEN);
+	} else {
+		is_ml_peer = false;
+		os_memcpy(peer_addr, data->external_auth.bssid, ETH_ALEN);
+	}
+
+	if (sme_set_sae_group(wpa_s, 0) < 0) {
+		wpa_printf(MSG_DEBUG, "EPPKE: Failed to select group");
+		return -1;
+	}
+
+	return wpas_pasn_auth_start(wpa_s, wpa_s->own_addr, peer_addr,
+				    rsn_key_mgmt_to_wpa_akm(
+					    data->external_auth.key_mgmt_suite),
+				    data->external_auth.pairwise_cipher,
+				    wpa_s->sme.sae.group, 0, NULL, 0,
+				    data->external_auth.auth_alg,
+				    data->external_auth.group_cipher,
+				    data->external_auth.group_mgmt_cipher,
+				    data->external_auth.rsn_capab,
+				    data->external_auth.rsnxe_data,
+				    is_ml_peer);
+}
+#endif /* CONFIG_ENC_ASSOC */
+
+
 void sme_external_auth_trigger(struct wpa_supplicant *wpa_s,
 			       union wpa_event_data *data)
 {
@@ -2222,6 +2257,14 @@ void sme_external_auth_trigger(struct wpa_supplicant *wpa_s,
 		} else {
 			wpa_s->sme.ext_ml_auth = false;
 		}
+#ifdef CONFIG_ENC_ASSOC
+		if (data->external_auth.auth_alg == WLAN_AUTH_EPPKE &&
+		    sme_handle_eppke_external_auth_start(wpa_s, data) < 0) {
+			sme_send_external_auth_status(
+				wpa_s, WLAN_STATUS_UNSPECIFIED_FAILURE);
+			return;
+		}
+#endif /* CONFIG_ENC_ASSOC */
 		wpa_s->sme.seq_num = 0;
 		wpa_s->sme.sae.state = SAE_NOTHING;
 		wpa_s->sme.sae.send_confirm = 0;
@@ -2691,6 +2734,20 @@ void sme_external_auth_mgmt_rx(struct wpa_supplicant *wpa_s,
 				    wpa_s->sme.ext_auth_ap_mld_addr :
 				    wpa_s->sme.ext_auth_bssid) < 0)
 			return;
+#ifdef CONFIG_ENC_ASSOC
+	} else if (le_to_host16(header->u.auth.auth_alg) == WLAN_AUTH_EPPKE) {
+		int res = wpas_pasn_auth_rx(wpa_s, header, len);
+
+		if (res < 0) {
+			/* Notify failure to the driver */
+			sme_send_external_auth_status(
+				wpa_s,
+				res == -2 ?
+				le_to_host16(header->u.auth.status_code) :
+				WLAN_STATUS_UNSPECIFIED_FAILURE);
+			return;
+		}
+#endif /* CONFIG_ENC_ASSOC */
 	}
 }
 

src/common/defs.h
@@ -207,6 +207,11 @@ static inline int wpa_key_mgmt_cross_akm(int akm)
 			 WPA_KEY_MGMT_SAE_EXT_KEY));
 }
 
+static inline bool wpa_key_mgmt_eppke(int akm)
+{
+	return !!(akm & WPA_KEY_MGMT_EPPKE);
+}
+
 #define WPA_PROTO_WPA BIT(0)
 #define WPA_PROTO_RSN BIT(1)
 #define WPA_PROTO_WAPI BIT(2)

src/drivers/driver.h
@@ -1432,6 +1432,11 @@ struct wpa_driver_associate_params {
 	 * p2p_mode - P2P R1 only, P2P R2 only, or PCC mode
 	 */
 	enum wpa_p2p_mode p2p_mode;
+
+	/**
+	 * eppke_supported - Whether EPPKE authentication is supported
+	 */
+	bool eppke_supported;
 };
 
 enum hide_ssid {

src/drivers/driver_nl80211.c
@@ -7608,6 +7608,15 @@ static int nl80211_connect_ext(struct i802_bss *bss,
 		connect_ext_feature_set(features, QCA_CONNECT_EXT_FEATURE_RSNO);
 	}
 
+#ifdef CONFIG_ENC_ASSOC
+	if (params->eppke_supported) {
+		wpa_printf(MSG_DEBUG,
+			   "- EPPKE external authentication support");
+		connect_ext_feature_set(features,
+					QCA_CONNECT_EXT_FEATURE_EXT_AUTH_EPPKE);
+	}
+#endif /* CONFIG_ENC_ASSOC */
+
 	if (nla_put(msg, QCA_WLAN_VENDOR_ATTR_CONNECT_EXT_FEATURES,
 		    sizeof(features), features))
 		goto fail;
@@ -7722,6 +7731,8 @@ static int wpa_driver_nl80211_try_connect(
 	if (params->auth_alg & WPA_AUTH_ALG_FT)
 		algs++;
 	if (params->auth_alg & WPA_AUTH_ALG_SAE)
+		algs++;
+	if (params->auth_alg & WPA_AUTH_ALG_EPPKE)
 		algs++;
 	if (algs > 1) {
 		wpa_printf(MSG_DEBUG, "  * Leave out Auth Type for automatic "

wpa_supplicant/events.c
@@ -7170,7 +7170,8 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 #ifdef CONFIG_SAE
 			if (stype == WLAN_FC_STYPE_AUTH &&
 			    !(wpa_s->drv_flags & WPA_DRIVER_FLAGS_SME) &&
-			    (wpa_s->drv_flags & WPA_DRIVER_FLAGS_SAE)) {
+			    ((wpa_s->drv_flags & WPA_DRIVER_FLAGS_SAE) ||
+			    (wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_EPPKE))) {
 				sme_external_auth_mgmt_rx(
 					wpa_s, data->rx_mgmt.frame,
 					data->rx_mgmt.frame_len);

wpa_supplicant/pasn_supplicant.c
@@ -744,6 +744,28 @@ static void wpas_pasn_auth_start_cb(struct wpa_radio_work *work, int deinit)
 	if ((wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SPP_AMSDU) &&
 	    ieee802_11_rsnx_capab(rsnxe, WLAN_RSNX_CAPAB_SPP_A_MSDU))
 		capab |= BIT(WLAN_RSNX_CAPAB_SPP_A_MSDU);
+#ifdef CONFIG_ENC_ASSOC
+	if (awork->auth_alg == WLAN_AUTH_EPPKE) {
+		if (!ieee802_11_rsnx_capab(rsnxe, WLAN_RSNX_CAPAB_KEK_IN_PASN))
+		{
+			wpa_printf(MSG_INFO,
+				   "EPPKE: KEK_IN_PASN not set in AP RSNXE");
+			goto fail;
+		}
+		if (!ieee802_11_rsnx_capab(rsnxe,
+					   WLAN_RSNX_CAPAB_ASSOC_FRAME_ENCRYPTION)) {
+			wpa_printf(MSG_INFO,
+				   "EPPKE: ASSOC_FRAME_ENCRYPTION not set in AP RSNXE");
+			goto fail;
+		}
+		if (wpa_s->drv_flags2 &
+		    WPA_DRIVER_FLAGS2_ASSOCIATION_FRAME_ENCRYPTION) {
+			capab |= BIT(WLAN_RSNX_CAPAB_ASSOC_FRAME_ENCRYPTION);
+			capab |= BIT(WLAN_RSNX_CAPAB_KEK_IN_PASN);
+			pasn->derive_kek = true;
+		}
+	}
+#endif /* CONFIG_ENC_ASSOC */
 
 	pasn_set_rsnxe_caps(pasn, capab);
 	pasn_register_callbacks(pasn, wpa_s, wpas_pasn_send_mlme, NULL, NULL,
@@ -1062,6 +1084,15 @@ int wpas_pasn_auth_rx(struct wpa_supplicant *wpa_s,
 
 		if (pasn->pmksa_entry)
 			wpa_sm_set_cur_pmksa(wpa_s->wpa, pasn->pmksa_entry);
+
+		if (pasn->auth_alg == WLAN_AUTH_EPPKE) {
+#ifdef CONFIG_SME
+			os_memcpy(wpa_s->sme.sae.pmkid, pasn->sae.pmkid,
+				  PMKID_LEN);
+#endif /* CONFIG_SME */
+			wpa_sm_set_pmk(wpa_s->wpa, pasn->pmk, pasn->pmk_len,
+				       pasn->sae.pmkid, pasn->own_addr);
+		}
 	}
 
 	forced_memzero(pasn_get_ptk(pasn), sizeof(pasn->ptk));

wpa_supplicant/wpa_supplicant.c
@@ -4882,6 +4882,18 @@ static void wpas_start_assoc_cb(struct wpa_radio_work *work, int deinit)
 		}
 	}
 
+#ifdef CONFIG_ENC_ASSOC
+	if (wpa_key_mgmt_eppke(ssid->key_mgmt) &&
+	    wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_EPPKE) {
+		params.eppke_supported = true;
+		if (wpa_key_mgmt_sae_ext_key(params.key_mgmt_suite) ||
+		    wpa_key_mgmt_sae_ext_key(params.allowed_key_mgmts)) {
+			wpa_dbg(wpa_s, MSG_DEBUG, "EPPKE authentication");
+			params.auth_alg = WPA_AUTH_ALG_EPPKE;
+		}
+	}
+#endif /* CONFIG_ENC_ASSOC */
+
 	params.drop_unencrypted = use_crypt;
 
 	params.mgmt_frame_protection = wpas_get_ssid_pmf(wpa_s, ssid);

greearb/hostap-ct

commit 36d0a41d65045276544c50acd2f5ffa16954b734

EPPKE: Fix group selection for EPPKE authentication in SME-in-driver case
When SAE is wrapped inside PASN authentication, the group must be
selected from PASN configuration, not from SAE-specific state. Fix the
external auth start path to select the group from PASN configuration and
align the SAE group to it, as required by IEEE P802.11-REVmf/D2.1,
12.13.5, so that PASN authentication is started with the correct group.

Fixes: e9a52e9d6adc ("EPPKE: EPPKE authentication for SME-in-driver case")
Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

wpa_supplicant/sme.c
@@ -2267,6 +2267,7 @@ static int sme_handle_eppke_external_auth_start(struct wpa_supplicant *wpa_s,
 	const u8 *ssid_str = data->external_auth.ssid;
 	u8 peer_addr[ETH_ALEN];
 	bool is_ml_peer;
+	int group;
 
 	wpa_s->sme.ext_auth_wpa_ssid = NULL;
 	/* Find the network profil based on the selected SSID */
@@ -2293,8 +2294,18 @@ static int sme_handle_eppke_external_auth_start(struct wpa_supplicant *wpa_s,
 		os_memcpy(peer_addr, data->external_auth.bssid, ETH_ALEN);
 	}
 
-	if (sme_set_sae_group(wpa_s, 0) < 0) {
-		wpa_printf(MSG_DEBUG, "EPPKE: Failed to select group");
+	group = wpas_pasn_get_group(wpa_s, ssid, NULL);
+	if (!group) {
+		wpa_printf(MSG_DEBUG, "EPPKE: Failed to select PASN group");
+		return -1;
+	}
+
+	/* As per IEEE P802.11-REVmf/D2.1, 12.13.5, when SAE is wrapped within
+	 * PASN authentication, both shall use the same finite cyclic group.
+	 */
+	if (sae_set_group(&wpa_s->sme.sae, group) < 0) {
+		wpa_printf(MSG_INFO, "EPPKE: Failed to set SAE group %u",
+			   group);
 		return -1;
 	}
 
@@ -2302,7 +2313,7 @@ static int sme_handle_eppke_external_auth_start(struct wpa_supplicant *wpa_s,
 				    rsn_key_mgmt_to_wpa_akm(
 					    data->external_auth.key_mgmt_suite),
 				    data->external_auth.pairwise_cipher,
-				    wpa_s->sme.sae.group, ssid->id, NULL, 0,
+				    group, ssid->id, NULL, 0,
 				    data->external_auth.auth_alg,
 				    data->external_auth.group_cipher,
 				    data->external_auth.group_mgmt_cipher,