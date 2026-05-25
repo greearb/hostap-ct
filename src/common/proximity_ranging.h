greearb/hostap-ct

commit f45cc220e4bbe541ffdacd303e653594379311e1

PR: Update PR device configs and capabilities from driver
Fetch device configurations, capabilities, and supported channels for
ranging and store them in Proximity Ranging global context. This
includes propagation of Enhanced Distributed channel Access
(EDCA-802.11mc) based ranging capabilities as well as Non-Trigger Based
(NTB-802.11az) ranging capabilities.

This commit does not include the actual driver interface specific
changes to fill in the values.

Signed-off-by: Peddolla Harshavardhan Reddy <peddolla@qti.qualcomm.com>

src/common/proximity_ranging.h
@@ -31,12 +31,54 @@ struct pr_device {
 
 
 struct pr_config {
+	u8 pasn_type;
+
+	int preferred_ranging_role;
+
+	char country[3];
+
 	u8 dev_addr[ETH_ALEN];
 
 	/**
 	 * dev_name - Device Name
 	 */
 	char *dev_name;
+
+	bool edca_ista_support;
+
+	bool edca_rsta_support;
+
+	u8 edca_format_and_bw;
+
+	u8 max_tx_antenna;
+
+	u8 max_rx_antenna;
+
+	bool ntb_ista_support;
+
+	bool ntb_rsta_support;
+
+	bool secure_he_ltf;
+
+	u8 max_tx_ltf_repetations;
+
+	u8 max_rx_ltf_repetations;
+
+	u8 max_tx_ltf_total;
+
+	u8 max_rx_ltf_total;
+
+	u8 max_rx_sts_le_80;
+
+	u8 max_rx_sts_gt_80;
+
+	u8 max_tx_sts_le_80;
+
+	u8 max_tx_sts_gt_80;
+
+	u8 ntb_format_and_bw;
+
+	bool support_6ghz;
 
 	/**
 	 * cb_ctx - Context to use with callback functions

src/drivers/driver.h
@@ -2424,6 +2424,12 @@ struct wpa_driver_capa {
 #define WPA_DRIVER_FLAGS2_P2P_FEATURE_PCC_MODE	0x0000000004000000ULL
 /** Driver supports arbitrary channel width changes in AP mode */
 #define WPA_DRIVER_FLAGS2_AP_CHANWIDTH_CHANGE	0x0000000008000000ULL
+/** Driver supports FTM initiator functionality */
+#define WPA_DRIVER_FLAGS2_FTM_INITIATOR		0x0000000010000000ULL
+/** Driver supports non-trigger based ranging responder functionality */
+#define WPA_DRIVER_FLAGS2_NON_TRIGGER_BASED_RESPONDER   0x0000000020000000ULL
+/** Driver supports non-trigger based ranging initiator functionality */
+#define WPA_DRIVER_FLAGS2_NON_TRIGGER_BASED_INITIATOR	0x0000000040000000ULL
 
 	u64 flags2;
 
@@ -2549,6 +2555,22 @@ struct wpa_driver_capa {
 	/* Maximum number of bytes of extra IE(s) that can be added to Probe
 	 * Request frames */
 	size_t max_probe_req_ie_len;
+
+	/* EDCA based ranging capabilities */
+	u8 edca_format_and_bw;
+	u8 max_tx_antenna;
+	u8 max_rx_antenna;
+
+	/* NTB based ranging capabilities */
+	u8 ntb_format_and_bw;
+	u8 max_tx_ltf_repetations;
+	u8 max_rx_ltf_repetations;
+	u8 max_tx_ltf_total;
+	u8 max_rx_ltf_total;
+	u8 max_rx_sts_le_80;
+	u8 max_rx_sts_gt_80;
+	u8 max_tx_sts_le_80;
+	u8 max_tx_sts_gt_80;
 };
 
 

wpa_supplicant/pr_supplicant.c
@@ -15,7 +15,8 @@
 #include "pr_supplicant.h"
 
 
-int wpas_pr_init(struct wpa_global *global, struct wpa_supplicant *wpa_s)
+int wpas_pr_init(struct wpa_global *global, struct wpa_supplicant *wpa_s,
+		 const struct wpa_driver_capa *capa)
 {
 	struct pr_config pr;
 
@@ -27,6 +28,41 @@ int wpas_pr_init(struct wpa_global *global, struct wpa_supplicant *wpa_s)
 	os_memcpy(pr.dev_addr, wpa_s->own_addr, ETH_ALEN);
 	pr.cb_ctx = wpa_s;
 	pr.dev_name = wpa_s->conf->device_name;
+	pr.pasn_type = wpa_s->conf->pr_pasn_type;
+	pr.preferred_ranging_role = wpa_s->conf->pr_preferred_role;
+
+	pr.edca_ista_support = wpa_s->drv_flags2 &
+		WPA_DRIVER_FLAGS2_FTM_INITIATOR;
+	pr.edca_rsta_support = wpa_s->drv_flags &
+		WPA_DRIVER_FLAGS_FTM_RESPONDER;
+	pr.edca_format_and_bw = capa->edca_format_and_bw;
+	pr.max_rx_antenna = capa->max_rx_antenna;
+	pr.max_tx_antenna = capa->max_tx_antenna;
+
+	pr.ntb_ista_support = wpa_s->drv_flags2 &
+		WPA_DRIVER_FLAGS2_NON_TRIGGER_BASED_INITIATOR;
+	pr.ntb_rsta_support = wpa_s->drv_flags2 &
+		WPA_DRIVER_FLAGS2_NON_TRIGGER_BASED_RESPONDER;
+	pr.ntb_format_and_bw = capa->ntb_format_and_bw;
+	pr.max_tx_ltf_repetations = capa->max_tx_ltf_repetations;
+	pr.max_rx_ltf_repetations = capa->max_rx_ltf_repetations;
+	pr.max_tx_ltf_total = capa->max_tx_ltf_total;
+	pr.max_rx_ltf_total = capa->max_rx_ltf_total;
+	pr.max_rx_sts_le_80 = capa->max_rx_sts_le_80;
+	pr.max_rx_sts_gt_80 = capa->max_rx_sts_gt_80;
+	pr.max_tx_sts_le_80 = capa->max_tx_sts_le_80;
+	pr.max_tx_sts_gt_80 = capa->max_tx_sts_gt_80;
+
+	pr.support_6ghz = wpas_is_6ghz_supported(wpa_s, true);
+
+	pr.secure_he_ltf = wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SEC_LTF_STA;
+
+	if (wpa_s->conf->country[0] && wpa_s->conf->country[1]) {
+		os_memcpy(pr.country, wpa_s->conf->country, 2);
+		pr.country[2] = 0x04;
+	} else {
+		os_memcpy(pr.country, "XX\x04", 3);
+	}
 
 	global->pr = pr_init(&pr);
 	if (!global->pr) {

wpa_supplicant/config.c
@@ -5795,6 +5795,8 @@ static const struct global_parse_data global_fields[] = {
 	{ BIN(wfa_gen_capa_supp), 0 },
 	{ BIN(wfa_gen_capa_cert), 0 },
 	{ BOOL(disable_op_classes_80_80_mhz), 0 },
+	{ INT(pr_pasn_type), 0 },
+	{ INT_RANGE(pr_preferred_role, 0, 1), 0},
 	/* NOTE: When adding new parameters here, add_interface() in
 	 * wpa_supplicant/dbus_new_introspect.c may need to be modified to
 	 * increase the size of the iface->xml buffer. */

wpa_supplicant/config.h
@@ -1909,6 +1909,15 @@ struct wpa_config {
 	 * supports this.
 	*/
 	bool disable_op_classes_80_80_mhz;
+
+	/* Indicates the types of PASN supported for Proximity Ranging */
+	int pr_pasn_type;
+
+	/* Indicates the preferred Proximity Ranging Role
+	 * 0: Prefer ranging initiator role (default)
+	 * 1: Prefer ranging responder role
+	 */
+	int pr_preferred_role;
 };
 
 

wpa_supplicant/config_file.c
@@ -1766,6 +1766,10 @@ static void wpa_config_write_global(FILE *f, struct wpa_config *config)
 	if (config->disable_op_classes_80_80_mhz)
 		fprintf(f, "disable_op_classes_80_80_mhz=%d\n",
 			config->disable_op_classes_80_80_mhz);
+	if (config->pr_pasn_type)
+		fprintf(f, "pr_pasn_type=%d\n", config->pr_pasn_type);
+	if (config->pr_preferred_role)
+		fprintf(f, "pr_preferred_role=%d\n", config->pr_preferred_role);
 }
 
 static void wpa_config_write_identity(FILE *f, struct wpa_dev_ik *dev_ik)

wpa_supplicant/pr_supplicant.h
@@ -11,13 +11,15 @@
 
 #ifdef CONFIG_PR
 
-int wpas_pr_init(struct wpa_global *global, struct wpa_supplicant *wpa_s);
+int wpas_pr_init(struct wpa_global *global, struct wpa_supplicant *wpa_s,
+		 const struct wpa_driver_capa *capa);
 void wpas_pr_deinit(struct wpa_supplicant *wpa_s);
 
 #else /* CONFIG_PR */
 
 static inline int wpas_pr_init(struct wpa_global *global,
-			       struct wpa_supplicant *wpa_s)
+			       struct wpa_supplicant *wpa_s,
+			       const struct wpa_driver_capa *capa)
 {
 	return -1;
 }

wpa_supplicant/wpa_supplicant.c
@@ -7972,7 +7972,7 @@ static int wpa_supplicant_init_iface(struct wpa_supplicant *wpa_s,
 		return -1;
 	}
 
-	if (wpas_pr_init(wpa_s->global, wpa_s) < 0)
+	if (wpas_pr_init(wpa_s->global, wpa_s, &capa) < 0)
 		return -1;
 
 	if (wpa_bss_init(wpa_s) < 0)

commit 7d81d336c2f14cca4350a10756fe1f1bf81b18e7

PR: Replace format_and_bw attribute with preamble/BW bitmaps
The PD ranging capability used a single format-and-bandwidth attribute
in wpa_driver_capa to represent the best supported combination. This
only considered the highest supported bandwidth and assumed all lower
bandwidths were also supported, which is not always the case. The
channel population logic inherited the same assumption.

Replace the attribute with separate preamble and bandwidth bitmaps so
each supported combination is explicitly represented. Parse the
corresponding nl80211 attributes to populate these bitmaps and remove
the now redundant attribute from driver.h. Update the channel population
logic to check each operating class bandwidth directly against the
bitmap rather than inferring support from the highest supported value.

Fixes: f45cc220e4bb ("PR: Update PR device configs and capabilities from driver")
Fixes: cfe7a215db0b ("PR: Determine channels that are supported to perform ranging")
Signed-off-by: Peddolla Harshavardhan Reddy <peddolla.reddy@oss.qualcomm.com>

src/common/proximity_ranging.h
@@ -98,6 +98,7 @@ enum edca_format_and_bw_value {
 	EDCA_FORMAT_AND_BW_VHT80P80 = 14,
 	EDCA_FORMAT_AND_BW_VHT160_DUAL_LO = 15,
 	EDCA_FORMAT_AND_BW_VHT160_SINGLE_LO = 16,
+	EDCA_FORMAT_AND_BW_INVALID
 };
 
 /**
@@ -114,6 +115,7 @@ enum ntb_format_and_bw_value {
 	NTB_FORMAT_AND_BW_HE80P80 = 3,
 	NTB_FORMAT_AND_BW_HE160_DUAL_LO = 4,
 	NTB_FORMAT_AND_BW_HE160_SINGLE_LO = 5,
+	NTB_FORMAT_AND_BW_INVALID
 };
 
 struct pr_capabilities {
@@ -382,6 +384,7 @@ struct pr_config {
 
 	bool edca_rsta_support;
 
+	/* Best single format_bw value derived from pd_format_bw_bitmap */
 	u8 edca_format_and_bw;
 
 	u32 edca_min_ranging_interval;
@@ -424,6 +427,11 @@ struct pr_config {
 
 	u8 max_tx_sts_gt_80;
 
+	/* PD ranging preamble and bandwidth bitmaps (shared by EDCA and NTB) */
+	u32 pd_preamble_bitmap;
+	u32 pd_format_bw_bitmap;
+
+	/* Best single format_bw value derived from pd_format_bw_bitmap */
 	u8 ntb_format_and_bw;
 
 	u32 ntb_min_ranging_interval;

src/drivers/driver.h
@@ -2266,6 +2266,18 @@ struct nan_capa {
 	bool he_valid;
 };
 
+/* Bit index values for pd_bandwidths bitmap */
+#define WPA_PR_CHAN_WIDTH_20	0
+#define WPA_PR_CHAN_WIDTH_40	1
+#define WPA_PR_CHAN_WIDTH_80	2
+#define WPA_PR_CHAN_WIDTH_80P80	3
+#define WPA_PR_CHAN_WIDTH_160	4
+
+/* Bit index values for pd_preambles bitmap */
+#define WPA_PR_PREAMBLE_HT	0
+#define WPA_PR_PREAMBLE_VHT	1
+#define WPA_PR_PREAMBLE_HE	2
+
 /**
  * struct wpa_driver_capa - Driver capability information
  */
@@ -2677,13 +2689,11 @@ struct wpa_driver_capa {
 	size_t max_probe_req_ie_len;
 
 	/* EDCA based ranging capabilities */
-	u8 edca_format_and_bw;
 	u8 max_tx_antenna;
 	u8 max_rx_antenna;
 	u32 edca_min_ranging_interval;
 
 	/* NTB based ranging capabilities */
-	u8 ntb_format_and_bw;
 	u8 max_tx_ltf_repetations;
 	u8 max_rx_ltf_repetations;
 	u8 max_tx_ltf_total;

src/drivers/driver_nl80211_capa.c
@@ -1139,6 +1139,38 @@ out:
 
 #ifdef CONFIG_PR
 
+static u32 nl80211_to_pd_bw_bitmap(u32 nl_bitmap)
+{
+	u32 bandwidths = 0;
+
+	if (nl_bitmap & BIT(NL80211_CHAN_WIDTH_20))
+		bandwidths |= BIT(WPA_PR_CHAN_WIDTH_20);
+	if (nl_bitmap & BIT(NL80211_CHAN_WIDTH_40))
+		bandwidths |= BIT(WPA_PR_CHAN_WIDTH_40);
+	if (nl_bitmap & BIT(NL80211_CHAN_WIDTH_80))
+		bandwidths |= BIT(WPA_PR_CHAN_WIDTH_80);
+	if (nl_bitmap & BIT(NL80211_CHAN_WIDTH_80P80))
+		bandwidths |= BIT(WPA_PR_CHAN_WIDTH_80P80);
+	if (nl_bitmap & BIT(NL80211_CHAN_WIDTH_160))
+		bandwidths |= BIT(WPA_PR_CHAN_WIDTH_160);
+	return bandwidths;
+}
+
+
+static u32 nl80211_to_pd_preamble_bitmap(u32 nl_bitmap)
+{
+	u32 preambles = 0;
+
+	if (nl_bitmap & BIT(NL80211_PREAMBLE_HT))
+		preambles |= BIT(WPA_PR_PREAMBLE_HT);
+	if (nl_bitmap & BIT(NL80211_PREAMBLE_VHT))
+		preambles |= BIT(WPA_PR_PREAMBLE_VHT);
+	if (nl_bitmap & BIT(NL80211_PREAMBLE_HE))
+		preambles |= BIT(WPA_PR_PREAMBLE_HE);
+	return preambles;
+}
+
+
 static void pmsr_type_ftm_handler(struct wpa_driver_nl80211_data *drv,
 				  struct nlattr *capa)
 {
@@ -1244,11 +1276,11 @@ static void pmsr_type_ftm_handler(struct wpa_driver_nl80211_data *drv,
 	drv->capa.support_6ghz = !!tb[NL80211_PMSR_FTM_CAPA_ATTR_6GHZ_SUPPORT];
 
 	if (tb[NL80211_PMSR_FTM_CAPA_ATTR_PD_PREAMBLES])
-		drv->capa.pd_preambles =
-			nla_get_u32(tb[NL80211_PMSR_FTM_CAPA_ATTR_PD_PREAMBLES]);
+		drv->capa.pd_preambles = nl80211_to_pd_preamble_bitmap(
+			nla_get_u32(tb[NL80211_PMSR_FTM_CAPA_ATTR_PD_PREAMBLES]));
 	if (tb[NL80211_PMSR_FTM_CAPA_ATTR_PD_BANDWIDTHS])
-		drv->capa.pd_bandwidths =
-			nla_get_u32(tb[NL80211_PMSR_FTM_CAPA_ATTR_PD_BANDWIDTHS]);
+		drv->capa.pd_bandwidths = nl80211_to_pd_bw_bitmap(
+			nla_get_u32(tb[NL80211_PMSR_FTM_CAPA_ATTR_PD_BANDWIDTHS]));
 }
 
 

wpa_supplicant/pr_supplicant.c
@@ -27,115 +27,116 @@ static void wpas_pr_pasn_timeout(void *eloop_ctx, void *timeout_ctx);
 #endif /* CONFIG_PASN */
 
 
-static int wpas_pr_edca_get_bw(enum edca_format_and_bw_value format_and_bw)
+static u8 wpas_pr_best_edca_format_bw(u32 bw_bitmap, u32 preamble_bitmap)
 {
-	switch (format_and_bw) {
-	case EDCA_FORMAT_AND_BW_VHT20:
-		return 20;
-	case EDCA_FORMAT_AND_BW_HT40:
-	case EDCA_FORMAT_AND_BW_VHT40:
-		return 40;
-	case EDCA_FORMAT_AND_BW_VHT80:
-		return 80;
-	case EDCA_FORMAT_AND_BW_VHT80P80:
-	case EDCA_FORMAT_AND_BW_VHT160_DUAL_LO:
-	case EDCA_FORMAT_AND_BW_VHT160_SINGLE_LO:
-		return 160;
-	default:
-		return 0;
-	}
+	/* Prefer highest bandwidth first */
+	if ((bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_160)) &&
+	    (preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT)))
+		return EDCA_FORMAT_AND_BW_VHT160_DUAL_LO;
+	if ((bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80P80)) &&
+	    (preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT)))
+		return EDCA_FORMAT_AND_BW_VHT80P80;
+	if ((bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80)) &&
+	    (preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT)))
+		return EDCA_FORMAT_AND_BW_VHT80;
+	if ((bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_40)) &&
+	    (preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT)))
+		return EDCA_FORMAT_AND_BW_VHT40;
+	if ((bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_40)) &&
+	    (preamble_bitmap & BIT(WPA_PR_PREAMBLE_HT)))
+		return EDCA_FORMAT_AND_BW_HT40;
+	if ((bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_20)) &&
+	    (preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT)))
+		return EDCA_FORMAT_AND_BW_VHT20;
+	return EDCA_FORMAT_AND_BW_INVALID;
 }
 
 
-static int wpas_pr_ntb_get_bw(enum ntb_format_and_bw_value format_and_bw)
+static u8 wpas_pr_best_ntb_format_bw(u32 bw_bitmap, u32 preamble_bitmap)
 {
-	switch (format_and_bw) {
-	case NTB_FORMAT_AND_BW_HE20:
-		return 20;
-	case NTB_FORMAT_AND_BW_HE40:
-		return 40;
-	case NTB_FORMAT_AND_BW_HE80:
-		return 80;
-	case NTB_FORMAT_AND_BW_HE80P80:
-	case NTB_FORMAT_AND_BW_HE160_DUAL_LO:
-	case NTB_FORMAT_AND_BW_HE160_SINGLE_LO:
-		return 160;
-	default:
-		return 0;
-	}
+	if (!(preamble_bitmap & BIT(WPA_PR_PREAMBLE_HE)))
+		return NTB_FORMAT_AND_BW_INVALID;
+
+	if (bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_160))
+		return NTB_FORMAT_AND_BW_HE160_SINGLE_LO;
+	if (bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80P80))
+		return NTB_FORMAT_AND_BW_HE80P80;
+	if (bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80))
+		return NTB_FORMAT_AND_BW_HE80;
+	if (bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_40))
+		return NTB_FORMAT_AND_BW_HE40;
+	if (bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_20))
+		return NTB_FORMAT_AND_BW_HE20;
+	return NTB_FORMAT_AND_BW_INVALID;
 }
 
 
 static bool
-wpas_pr_edca_is_valid_op_class(enum edca_format_and_bw_value format_and_bw,
+wpas_pr_edca_is_valid_op_class(u32 bw_bitmap, u32 preamble_bitmap,
 			       const struct oper_class_map *op_class_map)
 {
-	int bw = 0, op_class_bw = 0;
-
 	if (!op_class_map)
 		return false;
 
-	op_class_bw = oper_class_bw_to_int(op_class_map);
-	bw = wpas_pr_edca_get_bw(format_and_bw);
-
-	if (!op_class_bw || !bw)
+	switch (op_class_map->bw) {
+	case BW20:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_20)) &&
+			!!(preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT));
+	case BW40PLUS:
+	case BW40MINUS:
+	case BW40:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_40)) &&
+			!!(preamble_bitmap & (BIT(WPA_PR_PREAMBLE_VHT) |
+					      BIT(WPA_PR_PREAMBLE_HT)));
+	case BW80:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80)) &&
+			!!(preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT));
+	case BW80P80:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80P80)) &&
+			!!(preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT));
+	case BW160:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_160)) &&
+			!!(preamble_bitmap & BIT(WPA_PR_PREAMBLE_VHT));
+	default:
 		return false;
-
-	if (format_and_bw <= EDCA_FORMAT_AND_BW_VHT80 &&
-	    format_and_bw >= EDCA_FORMAT_AND_BW_VHT20 &&
-	    op_class_bw <= bw)
-		return true;
-
-	if (format_and_bw == EDCA_FORMAT_AND_BW_VHT80P80 &&
-	    (op_class_bw < bw || op_class_map->bw == BW80P80))
-		return true;
-
-	if ((format_and_bw == EDCA_FORMAT_AND_BW_VHT160_DUAL_LO ||
-	     format_and_bw == EDCA_FORMAT_AND_BW_VHT160_SINGLE_LO) &&
-	    (op_class_bw < bw || op_class_map->bw == BW160))
-		return true;
-
-	return false;
+	}
 }
 
 
 static bool
-wpas_pr_ntb_is_valid_op_class(enum ntb_format_and_bw_value format_and_bw,
+wpas_pr_ntb_is_valid_op_class(u32 bw_bitmap, u32 preamble_bitmap,
 			      const struct oper_class_map *op_class_map)
 {
-	int bw = 0, op_class_bw = 0;
-
 	if (!op_class_map)
 		return false;
 
-	op_class_bw = oper_class_bw_to_int(op_class_map);
-	bw = wpas_pr_ntb_get_bw(format_and_bw);
-
-	if (!op_class_bw || !bw)
+	/* NTB ranging requires HE preamble */
+	if (!(preamble_bitmap & BIT(WPA_PR_PREAMBLE_HE)))
 		return false;
 
-	if (format_and_bw <= NTB_FORMAT_AND_BW_HE80 &&
-	    format_and_bw >= NTB_FORMAT_AND_BW_HE20 &&
-	    op_class_bw <= bw)
-		return true;
-
-	if (format_and_bw == NTB_FORMAT_AND_BW_HE80P80 &&
-		   (op_class_bw < bw || op_class_map->bw == BW80P80))
-		return true;
-
-	if ((format_and_bw == NTB_FORMAT_AND_BW_HE160_DUAL_LO ||
-	     format_and_bw == NTB_FORMAT_AND_BW_HE160_SINGLE_LO) &&
-	    (op_class_bw < bw || op_class_map->bw == BW160))
-		return true;
-
-	return false;
+	switch (op_class_map->bw) {
+	case BW20:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_20));
+	case BW40PLUS:
+	case BW40MINUS:
+	case BW40:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_40));
+	case BW80:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80));
+	case BW80P80:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_80P80));
+	case BW160:
+		return !!(bw_bitmap & BIT(WPA_PR_CHAN_WIDTH_160));
+	default:
+		return false;
+	}
 }
 
 
 static void
 wpas_pr_setup_edca_channels(struct wpa_supplicant *wpa_s,
 			    struct pr_channels *chan,
-			    enum edca_format_and_bw_value format_and_bw)
+			    u32 bw_bitmap, u32 preamble_bitmap)
 {
 	struct hostapd_hw_modes *mode;
 	int cla = 0, i;
@@ -148,7 +149,8 @@ wpas_pr_setup_edca_channels(struct wpa_supplicant *wpa_s,
 		mode = get_mode(wpa_s->hw.modes, wpa_s->hw.num_modes, o->mode,
 				is_6ghz_op_class(o->op_class));
 		if (!mode || is_6ghz_op_class(o->op_class) ||
-		    !wpas_pr_edca_is_valid_op_class(format_and_bw, o))
+		    !wpas_pr_edca_is_valid_op_class(bw_bitmap, preamble_bitmap,
+						    o))
 			continue;
 
 		for (ch = o->min_chan; ch <= o->max_chan; ch += o->inc) {
@@ -194,7 +196,7 @@ wpas_pr_setup_edca_channels(struct wpa_supplicant *wpa_s,
 static void
 wpas_pr_setup_ntb_channels(struct wpa_supplicant *wpa_s,
 			   struct pr_channels *chan,
-			   enum ntb_format_and_bw_value format_and_bw,
+			   u32 bw_bitmap, u32 preamble_bitmap,
 			   bool allow_6ghz)
 {
 	int cla = 0, i;
@@ -208,7 +210,8 @@ wpas_pr_setup_ntb_channels(struct wpa_supplicant *wpa_s,
 		mode = get_mode(wpa_s->hw.modes, wpa_s->hw.num_modes, o->mode,
 				is_6ghz_op_class(o->op_class));
 		if (!mode || (!allow_6ghz && is_6ghz_op_class(o->op_class)) ||
-		    !wpas_pr_ntb_is_valid_op_class(format_and_bw, o))
+		    !wpas_pr_ntb_is_valid_op_class(bw_bitmap, preamble_bitmap,
+						   o))
 			continue;
 
 		for (ch = o->min_chan; ch <= o->max_chan; ch += o->inc) {
@@ -397,18 +400,28 @@ int wpas_pr_init(struct wpa_global *global, struct wpa_supplicant *wpa_s,
 		(int) (PR_PASN_DH19_UNAUTH | PR_PASN_DH19_AUTH);
 	pr.preferred_ranging_role = wpa_s->conf->pr_preferred_role;
 
-	pr.edca_ista_support = capa->ista.support_edca;
-	pr.edca_rsta_support = capa->rsta.support_edca;
-	pr.edca_format_and_bw = capa->edca_format_and_bw;
+	pr.edca_format_and_bw =
+		wpas_pr_best_edca_format_bw(capa->pd_bandwidths,
+					    capa->pd_preambles);
+	pr.edca_ista_support = capa->ista.support_edca &&
+		pr.edca_format_and_bw != EDCA_FORMAT_AND_BW_INVALID;
+	pr.edca_rsta_support = capa->rsta.support_edca &&
+		pr.edca_format_and_bw != EDCA_FORMAT_AND_BW_INVALID;
+	pr.pd_format_bw_bitmap = capa->pd_bandwidths;
+	pr.pd_preamble_bitmap = capa->pd_preambles;
 	pr.max_rx_antenna = capa->max_rx_antenna;
 	pr.max_tx_antenna = capa->max_tx_antenna;
 
 	wpas_pr_setup_edca_channels(wpa_s, &pr.edca_channels,
-				    pr.edca_format_and_bw);
-
-	pr.ntb_ista_support = capa->ista.support_ntb;
-	pr.ntb_rsta_support = capa->rsta.support_ntb;
-	pr.ntb_format_and_bw = capa->ntb_format_and_bw;
+				    capa->pd_bandwidths,
+				    capa->pd_preambles);
+	pr.ntb_format_and_bw =
+		wpas_pr_best_ntb_format_bw(capa->pd_bandwidths,
+					   capa->pd_preambles);
+	pr.ntb_ista_support = capa->ista.support_ntb &&
+		pr.ntb_format_and_bw != NTB_FORMAT_AND_BW_INVALID;
+	pr.ntb_rsta_support = capa->rsta.support_ntb &&
+		pr.ntb_format_and_bw != NTB_FORMAT_AND_BW_INVALID;
 	pr.max_tx_ltf_repetations = capa->max_tx_ltf_repetations;
 	pr.max_rx_ltf_repetations = capa->max_rx_ltf_repetations;
 	pr.max_tx_ltf_total = capa->max_tx_ltf_total;
@@ -438,7 +451,7 @@ int wpas_pr_init(struct wpa_global *global, struct wpa_supplicant *wpa_s,
 	pr.secure_he_ltf = wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SEC_LTF_STA;
 
 	wpas_pr_setup_ntb_channels(wpa_s, &pr.ntb_channels,
-				   pr.ntb_format_and_bw,
+				   capa->pd_bandwidths, capa->pd_preambles,
 				   pr.support_6ghz);
 
 	if (wpa_s->conf->country[0] && wpa_s->conf->country[1]) {