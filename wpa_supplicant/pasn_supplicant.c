greearb/hostap-ct

commit ae5cfa46b5b90d2c83f46aa8bd20d453b9026062

PASN: Fix SAE H2E capability advertisement for non-SAE AKMs
The WLAN_RSNX_CAPAB_SAE_H2E capability bit was unconditionally set in
wpas_pasn_auth_start_cb() regardless of the AKM in use. This bit should
only be advertised when an SAE EXT-KEY AKM is used.

Add a check for wpa_key_mgmt_sae_ext_key() before setting the SAE H2E
capability bit.

Fixes: 79f87f473412 ("PASN: Change PASN flows to use SAE H2E only")
Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

wpa_supplicant/pasn_supplicant.c
@@ -897,7 +897,8 @@ static void wpas_pasn_auth_start_cb(struct wpa_radio_work *work, int deinit)
 	pasn->corrupt_mic = wpa_s->conf->pasn_corrupt_mic;
 #endif /* CONFIG_TESTING_OPTIONS */
 
-	capab |= BIT(WLAN_RSNX_CAPAB_SAE_H2E);
+	if (wpa_key_mgmt_sae_ext_key(awork->akmp))
+		capab |= BIT(WLAN_RSNX_CAPAB_SAE_H2E);
 	if (wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SEC_LTF_STA)
 		capab |= BIT(WLAN_RSNX_CAPAB_SECURE_LTF);
 	if (wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SEC_RTT_STA)

commit 898e90f7bd59cff57b6f9d0b52c85bcf507b62c0

PASN: Fix SAE H2E capability bit advertisement
The WLAN_RSNX_CAPAB_SAE_H2E capability bit is required for all SAE AKMs
when used with PASN, not only for SAE EXT-KEY AKMs (which is required
for EPPKE). Update the condition to account for all SAE-based AKMs
accordingly.

Fixes: ae5cfa46b5b9 ("PASN: Fix SAE H2E capability advertisement for non-SAE AKMs")
Signed-off-by: Veerendranath Jakkam <vjakkam@qti.qualcomm.com>

wpa_supplicant/pasn_supplicant.c
@@ -897,7 +897,7 @@ static void wpas_pasn_auth_start_cb(struct wpa_radio_work *work, int deinit)
 	pasn->corrupt_mic = wpa_s->conf->pasn_corrupt_mic;
 #endif /* CONFIG_TESTING_OPTIONS */
 
-	if (wpa_key_mgmt_sae_ext_key(awork->akmp))
+	if (wpa_key_mgmt_sae(awork->akmp))
 		capab |= BIT(WLAN_RSNX_CAPAB_SAE_H2E);
 	if (wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SEC_LTF_STA)
 		capab |= BIT(WLAN_RSNX_CAPAB_SECURE_LTF);