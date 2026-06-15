greearb/hostap-ct

commit ff5248a8e91ec9cc0d1deff3b7aee85e4c4ab882

nl80211: Fix HE 6 GHz capability fetching on big-endian CPUs
nla_get_u16() returns the value in host byte order and there should be
no extra conversion from little-endian to host byte order.

Fixes: 9272ebae83e6 ("nl80211: Fetch HE 6 GHz capability from the driver")
Signed-off-by: Jouni Malinen <j@w1.fi>

src/drivers/driver_nl80211_capa.c
@@ -1993,12 +1993,9 @@ static void phy_info_iftype_copy(struct hostapd_hw_modes *mode,
 			  len);
 	}
 
-	if (tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA]) {
-		u16 capa;
-
-		capa = nla_get_u16(tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA]);
-		he_capab->he_6ghz_capa = le_to_host16(capa);
-	}
+	if (tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA])
+		he_capab->he_6ghz_capa =
+			nla_get_u16(tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA]);
 
 	if (!tb[NL80211_BAND_IFTYPE_ATTR_EHT_CAP_MAC] ||
 	    !tb[NL80211_BAND_IFTYPE_ATTR_EHT_CAP_PHY])

commit 00760f77483751a31e222b57b1966ac3757152ea

nl80211: Fix HE 6 GHz capability fetching
Originally, this used nla_get_u16() to a u16 variable and le_to_host16()
from that variable. That results in a static analysis warning about byte
order (le16 should have been used as the type and nla_get_u16() is
supposed to return the attribute value in host byte order). This was
then "fixed" by removing the call to le_to_host16() to get the types
matching normal nla_get_u16() uses. However, the attribute value is
actually the payload of the HE 6 GHz Band Capabilities element, not a
u16 integer. That element payload just happens to consists of a 16-bit
field that is in little endian byte order per IEEE 802.11 conventions.

Fix this by interpreting this attribute as variable length binary data
and accept it as valid if it has a length of two or more octets.
Byte-swap the first two octets to produce the host byte order value for
he_capab->he_6ghz_capa to match the initial implementation behavior.

Fixes: ff5248a8e91e ("nl80211: Fix HE 6 GHz capability fetching on big-endian CPUs")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

src/drivers/driver_nl80211_capa.c
@@ -2468,9 +2468,10 @@ static void phy_info_iftype_copy(struct hostapd_hw_modes *mode,
 			  len);
 	}
 
-	if (tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA])
-		he_capab->he_6ghz_capa =
-			nla_get_u16(tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA]);
+	if (tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA] &&
+	    nla_len(tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA]) >= 2)
+		he_capab->he_6ghz_capa = WPA_GET_LE16(
+			nla_data(tb[NL80211_BAND_IFTYPE_ATTR_HE_6GHZ_CAPA]));
 
 	if (!tb[NL80211_BAND_IFTYPE_ATTR_EHT_CAP_MAC] ||
 	    !tb[NL80211_BAND_IFTYPE_ATTR_EHT_CAP_PHY])