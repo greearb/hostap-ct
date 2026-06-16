greearb/hostap-ct

commit f991c23abc0e3d1421e1fb1f64bd7f92a9f38b02

EPPKE: Build RSNE for EPPKE in SME-in-driver mode
In SME-in-driver mode, all driver configured parameters are received in
the EVENT_EXTERNAL_AUTH event. Use these driver configured parameters to
build the RSNE for EPPKE in SME-in-driver mode.

Add a helper function wpa_external_auth_add_rsne() to generate the RSNE
and use it in wpas_eppke_set_rsne() during PASN startup for
WLAN_AUTH_EPPKE.

Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>
Signed-off-by: Ainy Kumari <ainy.kumari@oss.qualcomm.com>

src/rsn_supp/wpa_ie.c
@@ -302,6 +302,113 @@ int wpa_gen_wpa_ie(struct wpa_sm *sm, u8 *wpa_ie, size_t wpa_ie_len)
 }
 
 
+/*
+ * wpa_external_auth_add_rsne - Build an RSNE for external authentication
+ * @rsne: Buffer in which the RSNE will be written
+ * @rsne_len: Length of the RSNE buffer
+ * @sm: Pointer to WPA state machine data from wpa_sm_init()
+ * @akmp: Authentication and key management protocol
+ * @pairwise_cipher: The pairwise cipher suite
+ * @group_cipher: The group addressed data cipher suite
+ * @group_mgmt_cipher: The group addressed management cipher suite
+ * @rsn_capab: RSN capabilities field
+ * Returns: Length of the RSNE or -1 on failure
+ */
+int wpa_external_auth_add_rsne(u8 *rsne, size_t rsne_len, struct wpa_sm *sm,
+			       int akmp, int pairwise_cipher, int group_cipher,
+			       int group_mgmt_cipher, u16 rsn_capab)
+{
+	struct rsn_ie_hdr *hdr;
+	u32 suite;
+	u8 *pos;
+
+	wpa_printf(MSG_DEBUG, "RSN: Ext-Auth: Build RSNE");
+
+	if (rsne_len < sizeof(*hdr) + RSN_SELECTOR_LEN +
+	    2 + RSN_SELECTOR_LEN + 2 + RSN_SELECTOR_LEN + 2 +
+	    (sm->cur_pmksa ? 2 + PMKID_LEN : 0)) {
+		wpa_printf(MSG_DEBUG, "Ext-Auth: Too short RSNE buffer (%lu bytes)",
+			   (unsigned long) rsne_len);
+		return -1;
+	}
+
+	hdr = (struct rsn_ie_hdr *) rsne;
+	hdr->elem_id = WLAN_EID_RSN;
+	WPA_PUT_LE16(hdr->version, RSN_VERSION);
+	pos = (u8 *) (hdr + 1);
+
+	/* Group cipher */
+	suite = wpa_cipher_to_suite(WPA_PROTO_RSN, group_cipher);
+	if (!suite || !wpa_cipher_valid_group(group_cipher)) {
+		wpa_printf(MSG_INFO,
+			   "RSN: Ext-Auth: Invalid group cipher 0x%x",
+			   group_cipher);
+		return -1;
+	}
+	RSN_SELECTOR_PUT(pos, suite);
+	pos += RSN_SELECTOR_LEN;
+
+	/* Pairwise cipher */
+	WPA_PUT_LE16(pos, 1);
+	pos += 2;
+	suite = wpa_cipher_to_suite(WPA_PROTO_RSN, pairwise_cipher);
+	if (!suite ||
+	    (!wpa_cipher_valid_pairwise(pairwise_cipher) &&
+	     pairwise_cipher != WPA_CIPHER_NONE)) {
+		wpa_printf(MSG_INFO,
+			   "RSN: Ext-Auth: Invalid pairwise cipher 0x%x",
+			   pairwise_cipher);
+		return -1;
+	}
+	RSN_SELECTOR_PUT(pos, suite);
+	pos += RSN_SELECTOR_LEN;
+
+	/* AKM suite */
+	WPA_PUT_LE16(pos, 1);
+	pos += 2;
+	suite = wpa_akm_to_suite(akmp);
+	if (!suite) {
+		wpa_printf(MSG_INFO, "RSN: Ext-Auth: Invalid AKMP 0x%x", akmp);
+		return -1;
+	}
+	RSN_SELECTOR_PUT(pos, suite);
+	pos += RSN_SELECTOR_LEN;
+
+	/* RSN Capabilities */
+	WPA_PUT_LE16(pos, rsn_capab);
+	pos += 2;
+
+	if (sm->cur_pmksa) {
+		wpa_printf(MSG_DEBUG, "RSN: Ext-Auth: Adding PMKID");
+		/* PMKID Count (2 octets, little endian) */
+		WPA_PUT_LE16(pos, 1);
+		/* PMKID */
+		os_memcpy(pos, sm->cur_pmksa->pmkid, PMKID_LEN);
+		pos += PMKID_LEN;
+	}
+
+	/* Group Management Cipher Suite */
+	if (wpa_cipher_valid_mgmt_group(group_mgmt_cipher)) {
+		if (!sm->cur_pmksa) {
+			/* PMKID Count */
+			WPA_PUT_LE16(pos, 0);
+			pos += 2;
+		}
+
+		/* Management Group Cipher Suite */
+		RSN_SELECTOR_PUT(pos, wpa_cipher_to_suite(WPA_PROTO_RSN,
+							  group_mgmt_cipher));
+		pos += RSN_SELECTOR_LEN;
+	}
+
+	hdr->len = (pos - rsne) - 2;
+
+	WPA_ASSERT((size_t) (pos - rsne) <= rsne_len);
+
+	return pos - rsne;
+}
+
+
 int wpa_gen_rsnxe(struct wpa_sm *sm, u8 *rsnxe, size_t rsnxe_len)
 {
 	u8 *pos = rsnxe;

src/rsn_supp/wpa_ie.h
@@ -18,5 +18,9 @@ int wpa_gen_wpa_ie_rsn(u8 *rsn_ie, size_t rsn_ie_len,
 		       struct wpa_sm *sm);
 int wpa_gen_rsnxe(struct wpa_sm *sm, u8 *rsnxe, size_t rsnxe_len);
 u16 rsn_supp_capab(struct wpa_sm *sm);
+int wpa_external_auth_add_rsne(u8 *rsne, size_t rsne_len, struct wpa_sm *sm,
+			       int akmp, int pairwise_cipher,
+			       int group_cipher, int group_mgmt_cipher,
+			       u16 rsn_capab);
 
 #endif /* WPA_IE_H */

wpa_supplicant/pasn_supplicant.c
@@ -20,6 +20,7 @@
 #include "crypto/random.h"
 #include "eap_common/eap_defs.h"
 #include "rsn_supp/wpa.h"
+#include "rsn_supp/wpa_ie.h"
 #include "rsn_supp/pmksa_cache.h"
 #include "wpa_supplicant_i.h"
 #include "driver_i.h"
@@ -745,6 +746,40 @@ static struct wpa_bss * wpas_pasn_allowed(struct wpa_supplicant *wpa_s,
 }
 
 
+#ifdef CONFIG_ENC_ASSOC
+/*
+ * Build RSNE for EPPKE in SME-in-driver mode.
+ */
+static int wpas_eppke_set_rsne(struct wpa_supplicant *wpa_s,
+			       struct pasn_data *pasn,
+			       struct wpa_pasn_auth_work *awork)
+{
+	u8 rsne[257];
+	int rsne_len;
+
+	rsne_len = wpa_external_auth_add_rsne(rsne, sizeof(rsne), wpa_s->wpa,
+					      awork->akmp, awork->cipher,
+					      awork->group_cipher,
+					      awork->group_mgmt_cipher,
+					      awork->rsn_capab);
+	if (rsne_len < 0) {
+		wpa_printf(MSG_DEBUG, "EPPKE: Failed to build RSNE");
+		return -1;
+	}
+
+	pasn_set_rsne(pasn, rsne);
+	if (!pasn->rsn_ie)
+		return -1;
+
+	wpa_printf(MSG_DEBUG,
+		   "EPPKE: RSNE for ext-auth (group=0x%x mgmt=0x%x capab=0x%x)",
+		   awork->group_cipher, awork->group_mgmt_cipher,
+		   awork->rsn_capab);
+	return 0;
+}
+#endif /* CONFIG_ENC_ASSOC */
+
+
 static void wpas_pasn_auth_start_cb(struct wpa_radio_work *work, int deinit)
 {
 	struct wpa_supplicant *wpa_s = work->wpa_s;
@@ -952,6 +987,13 @@ static void wpas_pasn_auth_start_cb(struct wpa_radio_work *work, int deinit)
 	 */
 	if (awork->auth_alg == WLAN_AUTH_EPPKE && ssid)
 		pasn->network_ctx = ssid;
+
+	/* Build RSNE for EPPKE Authentication in SME-in-driver mode */
+	if (awork->auth_alg == WLAN_AUTH_EPPKE &&
+	    wpas_eppke_set_rsne(wpa_s, pasn, awork) < 0) {
+		wpa_printf(MSG_DEBUG, "EPPKE: Failed to configure RSNE");
+		goto fail;
+	}
 #endif /* CONFIG_ENC_ASSOC */
 
 	ret = wpas_pasn_start(pasn, awork->own_addr, awork->peer_addr,

commit d595bf6946c1c9bb0546816b0520dc12db4ea5b5

EPPKE: Fix PMKID written at wrong offset in wpa_external_auth_add_rsne()
The pos pointer was not advanced after writing the PMKID Count
field in wpa_external_auth_add_rsne(), causing the PMKID to
overwrite the count field. Fix this by advancing pos by 2 after
writing the PMKID Count.

Fixes: f991c23abc0e ("EPPKE: Build RSNE for EPPKE in SME-in-driver mode")
Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

src/rsn_supp/wpa_ie.c
@@ -382,6 +382,7 @@ int wpa_external_auth_add_rsne(u8 *rsne, size_t rsne_len, struct wpa_sm *sm,
 		wpa_printf(MSG_DEBUG, "RSN: Ext-Auth: Adding PMKID");
 		/* PMKID Count (2 octets, little endian) */
 		WPA_PUT_LE16(pos, 1);
+		pos += 2;
 		/* PMKID */
 		os_memcpy(pos, sm->cur_pmksa->pmkid, PMKID_LEN);
 		pos += PMKID_LEN;