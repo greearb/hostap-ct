mj22226/hostapd

commit 064d1b3728767ee4044294b2604215d0028e720e

SME: Skip RSN parameter selection when continuing authentication
There is no point in setting up RSN parameters again when continuing an
Authentication frame exchange beyond the first frame. In addition to
being unnecessary processing, this can cause issues for PMKSA caching
fallback by finding the same PMKSA cache entry again after the AP has
rejected its use.

Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/sme.c
@@ -1274,6 +1274,8 @@ static void sme_send_authentication(struct wpa_supplicant *wpa_s,
 	}
 #endif /* CONFIG_IEEE8021X_AUTH */
 
+	if (!start)
+		goto skip_setup;
 	if ((wpa_bss_get_vendor_ie(bss, WPA_IE_VENDOR_TYPE) ||
 	     wpa_bss_get_rsne(wpa_s, bss, ssid, false)) &&
 	    wpa_key_mgmt_wpa(ssid->key_mgmt)) {
@@ -1340,6 +1342,7 @@ static void sme_send_authentication(struct wpa_supplicant *wpa_s,
 		wpa_supplicant_set_non_wpa_policy(wpa_s, ssid);
 		wpa_s->sme.assoc_req_ie_len = 0;
 	}
+skip_setup:
 
 	/* In case the WPA vendor IE is used, it should be placed after all the
 	 * non-vendor IEs, as the lower layer expects the IEs to be ordered as

greearb/hostap-ct

commit 1aec83962bb7ac52ce5554aa546a92592eddc4da

SME: Fix duplicated Association Request IEs when continuing authentication
Commit 064d1b372876 skipped the RSN parameter selection when sending the
second or later Authentication frame of an exchange (e.g., the SAE
confirm). However, the skipped block was also what reset
sme.assoc_req_ie_len, while the IE construction after the skip_setup
label still ran for every Authentication frame. Each additional frame of
the exchange appended another copy of several elements (Supported
Operating Classes, RRM Enabled Capabilities, RSNXE, etc.) and inserted
another Extended Capabilities element after the RSNE.

As a result, any connection that uses more than one Authentication frame
with SME-in-wpa_supplicant (e.g., all SAE authentication with
mac80211-based drivers) sends an Association Request frame with
duplicated IEs. Found while debugging MLO association failures on rtw89
against a TP-Link Deco BE65 AP MLD, where the main branch sends each of
these elements twice in the two-link association request while
wpa_supplicant 2.11 sends them once.

Fix this by skipping element construction for the Association Request
frame entirely when continuing an Authentication frame exchange. The
elements were already built when the first frame of the exchange was
sent and do not change within the exchange.

Fixes: 064d1b372876 ("SME: Skip RSN parameter selection when continuing authentication")
Signed-off-by: Louis Kotze <loukot@gmail.com>

wpa_supplicant/sme.c
@@ -1439,7 +1439,6 @@ static void sme_send_authentication(struct wpa_supplicant *wpa_s,
 		wpa_supplicant_set_non_wpa_policy(wpa_s, ssid);
 		wpa_s->sme.assoc_req_ie_len = 0;
 	}
-skip_setup:
 
 	/* In case the WPA vendor IE is used, it should be placed after all the
 	 * non-vendor IEs, as the lower layer expects the IEs to be ordered as
@@ -1674,6 +1673,7 @@ skip_setup:
 	}
 #endif /* CONFIG_MBO */
 
+skip_setup:
 #ifdef CONFIG_ENC_ASSOC
 	if (!skip_auth && params.auth_alg == WPA_AUTH_ALG_EPPKE) {
 		if (start) {