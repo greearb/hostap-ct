greearb/hostap-ct

commit da95d9da51a17d65f5255aa9b75b7c48ec1c3eb0

PR: Deliver received PR PASN auth frames for processing
If a Proximity Ranging element is present in the PASN frame, it is
processed by proximity ranging implementation. Deliver the frame there
instead of the generic PASN processing. The actual PR processing will be
added in separate commits.

Signed-off-by: Peddolla Harshavardhan Reddy <peddolla@qti.qualcomm.com>

wpa_supplicant/pr_supplicant.c
@@ -10,6 +10,7 @@
 
 #include "utils/common.h"
 #include "utils/eloop.h"
+#include "common/ieee802_11_defs.h"
 #include "common/proximity_ranging.h"
 #include "p2p/p2p.h"
 #include "wpa_supplicant_i.h"
@@ -536,6 +537,18 @@ int wpas_pr_pasn_auth_tx_status(struct wpa_supplicant *wpa_s, const u8 *data,
 		return -1;
 
 	return pr_pasn_auth_tx_status(pr, data, data_len, acked);
+}
+
+
+int wpas_pr_pasn_auth_rx(struct wpa_supplicant *wpa_s,
+			 const struct ieee80211_mgmt *mgmt, size_t len,
+			 int freq)
+{
+	struct pr_data *pr = wpa_s->global->pr;
+
+	if (!pr)
+		return -2;
+	return pr_pasn_auth_rx(pr, mgmt, len, freq);
 }
 
 #endif /* CONFIG_PASN */

src/common/ieee802_11_common.c
@@ -165,6 +165,11 @@ static int ieee802_11_parse_vendor_specific(const u8 *pos, size_t elen,
 			elems->p2p2_ie = pos;
 			elems->p2p2_ie_len = elen;
 			break;
+		case PR_OUI_TYPE:
+			/* Wi-Fi Alliance - Proximity Ranging element */
+			elems->proximity_ranging = pos;
+			elems->proximity_ranging_len = elen;
+			break;
 		default:
 			wpa_printf(MSG_MSGDUMP, "Unknown WFA "
 				   "information element ignored "

src/common/ieee802_11_common.h
@@ -122,6 +122,7 @@ struct ieee802_11_elems {
 	const u8 *rsnxe_override;
 	const u8 *rsn_selection;
 	const u8 *wfa_capab;
+	const u8 *proximity_ranging;
 
 	u8 ssid_len;
 	u8 supp_rates_len;
@@ -191,6 +192,7 @@ struct ieee802_11_elems {
 	size_t rsnxe_override_len;
 	size_t rsn_selection_len;
 	u8 wfa_capab_len;
+	size_t proximity_ranging_len;
 
 	struct mb_ies_info mb_ies;
 

src/common/proximity_ranging.c
@@ -1570,4 +1570,37 @@ int pr_pasn_auth_tx_status(struct pr_data *pr, const u8 *data, size_t data_len,
 	return ret;
 }
 
+
+int pr_pasn_auth_rx(struct pr_data *pr, const struct ieee80211_mgmt *mgmt,
+		    size_t len, int freq)
+{
+	struct pr_device *dev;
+	u16 auth_alg;
+
+	dev = pr_get_device(pr, mgmt->sa);
+	if (!dev) {
+		wpa_printf(MSG_INFO, "PR: Peer not found " MACSTR,
+			   MAC2STR(mgmt->sa));
+		return -1;
+	}
+
+	if (!ether_addr_equal(mgmt->da, pr->cfg->dev_addr)) {
+		wpa_printf(MSG_INFO, "PR PASN: Not our frame");
+		return -1;
+	}
+
+	if (len < offsetof(struct ieee80211_mgmt, u.auth.variable))
+		return -1;
+
+	auth_alg = le_to_host16(mgmt->u.auth.auth_alg);
+	if (auth_alg != WLAN_AUTH_PASN) {
+		wpa_printf(MSG_INFO,
+			   "PR: Unexpected Authentication frame, auth_alg=%d",
+			   auth_alg);
+		return -1;
+	}
+
+	return 0;
+}
+
 #endif /* CONFIG_PASN */

src/common/proximity_ranging.h
@@ -469,5 +469,7 @@ int pr_initiate_pasn_auth(struct pr_data *pr, const u8 *addr, int freq,
 			  int forced_pr_freq);
 int pr_pasn_auth_tx_status(struct pr_data *pr, const u8 *data, size_t data_len,
 			   bool acked);
+int pr_pasn_auth_rx(struct pr_data *pr, const struct ieee80211_mgmt *mgmt,
+		    size_t len, int freq);
 
 #endif /* PROXIMITY_RANGING_H */

wpa_supplicant/events.c
@@ -6205,7 +6205,6 @@ static int wpas_pasn_auth(struct wpa_supplicant *wpa_s,
 			  const struct ieee80211_mgmt *mgmt, size_t len,
 			  int freq)
 {
-#ifdef CONFIG_P2P
 	struct ieee802_11_elems elems;
 	size_t auth_length;
 
@@ -6231,9 +6230,14 @@ static int wpas_pasn_auth(struct wpa_supplicant *wpa_s,
 		return -2;
 	}
 
+#ifdef CONFIG_P2P
 	if (elems.p2p2_ie && elems.p2p2_ie_len)
 		return wpas_p2p_pasn_auth_rx(wpa_s, mgmt, len, freq);
 #endif /* CONFIG_P2P */
+#ifdef CONFIG_PR
+	if (elems.proximity_ranging && elems.proximity_ranging_len)
+		return wpas_pr_pasn_auth_rx(wpa_s, mgmt, len, freq);
+#endif /* CONFIG_PR */
 
 	return wpas_pasn_auth_rx(wpa_s, mgmt, len);
 }

wpa_supplicant/pr_supplicant.h
@@ -27,6 +27,9 @@ int wpas_pr_initiate_pasn_auth(struct wpa_supplicant *wpa_s,
 			       int forced_pr_freq);
 int wpas_pr_pasn_auth_tx_status(struct wpa_supplicant *wpa_s, const u8 *data,
 				size_t data_len, bool acked);
+int wpas_pr_pasn_auth_rx(struct wpa_supplicant *wpa_s,
+			 const struct ieee80211_mgmt *mgmt, size_t len,
+			 int freq);
 
 #else /* CONFIG_PR */
 
@@ -68,6 +71,13 @@ static inline int wpas_pr_initiate_pasn_auth(struct wpa_supplicant *wpa_s,
 static inline int wpas_pr_pasn_auth_tx_status(struct wpa_supplicant *wpa_s,
 					      const u8 *data, size_t data_len,
 					      bool acked)
+{
+	return 0;
+}
+
+static inline int wpas_pr_pasn_auth_rx(struct wpa_supplicant *wpa_s,
+				       const struct ieee80211_mgmt *mgmt,
+				       size_t len, int freq)
 {
 	return 0;
 }

commit 04c6c342655a734ea8678aa414f01854e20ee11f

PR: Clean up session state on PASN auth RX failure
When pr_pasn_auth_rx() returns an error during PASN frame processing,
the PASN timeout, auth radio work, PD wdev, and ranging params are
left active. This prevents subsequent PR_PASN_START requests from
succeeding and blocks other radio work.

Cancel the PASN timeout, release the auth radio work, stop the PD wdev,
and clear the ranging params when pr_pasn_auth_rx() returns a negative
value.

Fixes: da95d9da51a1 ("PR: Deliver received PR PASN auth frames for processing")
Signed-off-by: Peddolla Harshavardhan Reddy <peddolla.reddy@oss.qualcomm.com>

wpa_supplicant/pr_supplicant.c
@@ -1648,6 +1648,7 @@ int wpas_pr_pasn_auth_rx(struct wpa_supplicant *wpa_s,
 			 int freq)
 {
 	struct pr_data *pr = wpa_s->global->pr;
+	int ret;
 
 	if (!pr)
 		return -2;
@@ -1695,7 +1696,16 @@ int wpas_pr_pasn_auth_rx(struct wpa_supplicant *wpa_s,
 		}
 	}
 
-	return pr_pasn_auth_rx(pr, mgmt, len, freq);
+	ret = pr_pasn_auth_rx(pr, mgmt, len, freq);
+
+	if (ret < 0) {
+		eloop_cancel_timeout(wpas_pr_pasn_timeout, wpa_s, NULL);
+		wpas_pr_pasn_auth_work_done(wpa_s);
+		wpas_pr_pd_stop(wpa_s);
+		wpas_pr_clear_ranging_params(wpa_s->global->pr);
+	}
+
+	return ret;
 }
 
 