greearb/hostap-ct

commit 6f6f9a941c2a5189b7cb9c047546a08b2b6df0cb

nl80211: Set assoc_freq for non-AP MLD
drv->assoc_freq was not set on non-AP MLD (STA) during ML association.
As a result, the wpa_cli status command reports a frequency value of 0
for the associated non-AP MLD.

Fix this by setting drv->assoc_freq for non-AP MLD cases as well.

Signed-off-by: Aaradhana Sahu <aaradhana.sahu@oss.qualcomm.com>

src/drivers/driver_nl80211.c
@@ -6963,14 +6963,16 @@ static int nl80211_connect_common(struct wpa_driver_nl80211_data *drv,
 			return -1;
 	}
 
-	if (params->freq.freq && !params->mld_params.mld_addr) {
-		wpa_printf(MSG_DEBUG, "  * freq=%d", params->freq.freq);
-		if (nla_put_u32(msg, NL80211_ATTR_WIPHY_FREQ,
-				params->freq.freq))
-			return -1;
+	if (params->freq.freq) {
+		if (!params->mld_params.mld_addr) {
+			wpa_printf(MSG_DEBUG, "  * freq=%d", params->freq.freq);
+			if (nla_put_u32(msg, NL80211_ATTR_WIPHY_FREQ,
+					params->freq.freq))
+				return -1;
+		}
+
 		drv->assoc_freq = params->freq.freq;
-	} else
-		drv->assoc_freq = 0;
+	}
 
 	if (params->freq_hint) {
 		wpa_printf(MSG_DEBUG, "  * freq_hint=%d", params->freq_hint);

commit b0ead4efaa4fc144f15b4a7f6805afaabf49de61

nl80211: Fix stale assoc_freq causing WPA3 cross-band roaming failure
When driver BSS selection is enabled, params->freq.freq is 0 in
nl80211_connect_common(). Commit 6f6f9a941c2a removed the else branch
that resets drv->assoc_freq to 0 in this case, leaving it with the
previous BSS frequency. During cross-band roaming,
nl80211_get_assoc_freq() returns the stale value, causing send_mlme() to
transmit SAE Authentication frames on the wrong channel and roaming
fails.

Restore the else branch to reset drv->assoc_freq = 0 when
params->freq.freq is not specified.

Fixes: 6f6f9a941c2a ("nl80211: Set assoc_freq for non-AP MLD")
Signed-off-by: Shelley Yang <shelley.yang@infineon.com>

src/drivers/driver_nl80211.c
@@ -7573,6 +7573,8 @@ static int nl80211_connect_common(struct wpa_driver_nl80211_data *drv,
 		}
 
 		drv->assoc_freq = params->freq.freq;
+	} else {
+		drv->assoc_freq = 0;
 	}
 
 	if (params->freq_hint) {