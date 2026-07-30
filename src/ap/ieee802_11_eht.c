2187483e3c736120dec451a3e769f80f166d1b7a
Jouni Malinen <jouni.malinen@oss.qualcomm.com>
2026-07-30 12:05:39 +0300

    AP MLD: Fix error path handling for link reconfiguration
    The case of no new association STA being found used incorrect STA
    pointer (affiliated link instead of the previously used association
    link) and that would have resulted in the sta entry being freed twice
    (once as a different link STA and once as the assoc STA in
    hostapd_notif_disassoc_mld(). However, this case might not be reachable
    since hostapd rejects a request to delete all links.
    
    In any case, it is better to be more careful here should this become
    reachable. Avoid double freeing of the STA entry by using the
    appropriate assoc_sta pointer in the hostapd_notif_disassoc_mld() call
    and skip the "goto exit" processing for this case to avoid another
    double freeing
    
    Fixes: ea7af69a2369 ("AP MLD: Process TX status for Link Reconfiguration Response frame")
    Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

--- src/ap/ieee802_11_eht.c
+++ src/ap/ieee802_11_eht.c
@@ -1736,9 +1736,10 @@ void hostapd_link_reconf_resp_tx_status(struct hostapd_data *hapd,
 			if (i == MAX_NUM_MLD_LINKS) {
 				wpa_printf(MSG_INFO,
 					   "MLD: No new assoc STA could be found; disconnect STA");
-				hostapd_notif_disassoc_mld(assoc_hapd, sta,
-							   sta->addr);
-				goto exit;
+				hostapd_notif_disassoc_mld(assoc_hapd,
+							   assoc_sta,
+							   assoc_sta->addr);
+				return;
 			}
 			wpa_printf(MSG_DEBUG, "MLD: New assoc link=%d", i);
 
