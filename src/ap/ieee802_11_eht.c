greearb/hostap-ct

commit 5b8ad4aa16378e5435a53c9ce6d2e995d7c27ed2

AP MLD: Add more link reconfig request details into debug log
Hexdump various Reconfiguration MLE fields into debug log to make it
easier to follow what exactly is requested.

Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

src/ap/ieee802_11_eht.c
@@ -2207,6 +2207,9 @@ hostapd_parse_link_reconf_req_sta_profile(struct hostapd_data *hapd,
 	int ret = -1;
 	u16 status;
 
+	wpa_hexdump(MSG_MSGDUMP, "MLD: Link Reconfig Request subelement",
+		    buf, len);
+
 	if (len < sizeof(*elem) + 2UL)
 		goto out;
 
@@ -2309,6 +2312,7 @@ hostapd_parse_link_reconf_req_sta_profile(struct hostapd_data *hapd,
 	}
 
 	sta_info_len = *sta_info;
+	wpa_hexdump(MSG_MSGDUMP, "MLD: STA Info", sta_info + 1, sta_info_len);
 
 	os_memcpy(sta_addr, sta_info + 1, ETH_ALEN);
 	wpa_printf(MSG_DEBUG, "MLD: Link STA addr=" MACSTR, MAC2STR(sta_addr));
@@ -2384,6 +2388,10 @@ hostapd_parse_link_reconf_req_sta_profile(struct hostapd_data *hapd,
 	ret = 0;
 
 add_to_list:
+	if (sta_info && sta_prof_len)
+		wpa_hexdump(MSG_MSGDUMP, "MLD: STA Profile",
+			    sta_info, sta_prof_len);
+
 	info = os_zalloc(sizeof(struct link_reconf_req_info) + sta_prof_len);
 	if (!info) {
 		wpa_printf(MSG_DEBUG, "MLD: Failed to allocate request info");

commit 067132931183b9591e2bce90a008f70a752094d0

AP MLD: Fix debug printing of STA Info for link reconfiguration
The first octet of this field was skipped unintentially and that ended
up printing one octet beyond the end of the field as well.

Fixes: 5b8ad4aa1637 ("AP MLD: Add more link reconfig request details into debug log")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

src/ap/ieee802_11_eht.c
@@ -2313,7 +2313,7 @@ hostapd_parse_link_reconf_req_sta_profile(struct hostapd_data *hapd,
 	}
 
 	sta_info_len = *sta_info;
-	wpa_hexdump(MSG_MSGDUMP, "MLD: STA Info", sta_info + 1, sta_info_len);
+	wpa_hexdump(MSG_MSGDUMP, "MLD: STA Info", sta_info, sta_info_len);
 
 	os_memcpy(sta_addr, sta_info + 1, ETH_ALEN);
 	wpa_printf(MSG_DEBUG, "MLD: Link STA addr=" MACSTR, MAC2STR(sta_addr));