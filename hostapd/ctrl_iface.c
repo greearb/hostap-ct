f7dc07d807cea5c8493af1ac04482c3c600a2ad8
Ben Greear <greearb@candelatech.com>
2026-08-03 16:03:01 -0700

    hostap:  Fix compile warning.
    Some previous patch forgot a 'return 0'

--- hostapd/ctrl_iface.c
+++ hostapd/ctrl_iface.c
@@ -197,6 +197,7 @@ static int hostapd_ctrl_iface_update(struct hostapd_data *hapd, char *txt)
 	hostapd_reload_config(iface, 1);
 
 	iface->interfaces->config_read_cb = config_read_cb;
+	return 0;
 }
 
 #ifdef NEED_AP_MLME
